fb6886f5f5b336cbbd5229da127f1fceb678bdb3
tuyojr <olutuyod@gmail.com>
2022-12-16 13:12:31 +0100

    simple function to determine if a year is a leap year or not

--- leap_year.py
+++ leap_year.py
@@ -1,3 +1,4 @@
+# HACKER RANK
 # An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
 
 # In the Gregorian calendar, three conditions are used to identify leap years:
