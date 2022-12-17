9c9d6669479d039d75c61e4ff693042ee0ac9c5a
tuyojr <olutuyod@gmail.com>
2022-12-17 09:25:34 +0100

    sample tests

--- code_wars/simple_multiplication.py
+++ code_wars/simple_multiplication.py
@@ -1,2 +1,17 @@
 # This kata is about multiplying a given number by eight 
 # if it is an even number and by nine otherwise.
+
+
+
+# import codewars_test as test
+# from solution import simple_multiplication
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(simple_multiplication(2), 16)
+#         test.assert_equals(simple_multiplication(1), 9)
+#         test.assert_equals(simple_multiplication(8), 64)
+#         test.assert_equals(simple_multiplication(4), 32)
+#         test.assert_equals(simple_multiplication(5), 45)
