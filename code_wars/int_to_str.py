c42e755974c0aef5b5e99a62b916a51b95a56845
tuyojr <olutuyod@gmail.com>
2022-12-18 07:35:47 +0100

    sample tests

---
+++ code_wars/int_to_str.py
@@ -0,0 +1,23 @@
+# We need a function that can transform a number (integer) into a string.
+
+# What ways of achieving this do you know?
+
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+
+
+# import codewars_test as test
+# from solution import number_to_string
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(number_to_string(67), '67')
+#         test.assert_equals(number_to_string(79585), '79585')
+#         test.assert_equals(number_to_string(79585), "79585")
+#         test.assert_equals(number_to_string(1+2), '3')
+#         test.assert_equals(number_to_string(1-2), '-1')
