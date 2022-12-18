cd5912b9293036682fdfa3b6cf63358dd6a7ec82
tuyojr <olutuyod@gmail.com>
2022-12-18 07:41:00 +0100

    sample tests

--- code_wars/sum_arr.py
+++ code_wars/sum_arr.py
@@ -22,3 +22,19 @@
 # who are just learning loops and math operations.
 # Advanced users may find this extremely easy and can easily write this 
 # in one line.
+
+
+
+# import codewars_test as test
+# from solution import sum_array
+
+# @test.describe("Testing sum array")
+# def tests():
+#     @test.it("Fixed tests")
+#     def fixed_tests(): 
+#         test.assert_equals(sum_array([]), 0)
+#         test.assert_equals(sum_array([1, 2, 3]), 6)
+#         test.assert_equals(sum_array([1.1, 2.2, 3.3]), 6.6)
+#         test.assert_equals(sum_array([4, 5, 6]), 15)
+#         test.assert_equals(sum_array(range(101)), 5050)
+  
