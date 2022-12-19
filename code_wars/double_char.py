5c56e25c9db4f73a0d0fccdc2a81fcde82491eb8
tuyojr <olutuyod@gmail.com>
2022-12-19 08:00:12 +0100

    sample tests

--- code_wars/double_char.py
+++ code_wars/double_char.py
@@ -6,3 +6,9 @@
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
 # * "1234!_ "     -> "11223344!!__  "
 # Good Luck!
+
+
+
+# test.assert_equals(double_char("String"),"SSttrriinngg")
+# test.assert_equals(double_char("Hello World"),"HHeelllloo  WWoorrlldd")
+# test.assert_equals(double_char("1234!_ "),"11223344!!__  ")
