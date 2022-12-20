68806786f423c4fb2cf55034908531ef0b2e2124
tuyojr <olutuyod@gmail.com>
2022-12-20 07:48:21 +0100

    sample tests

--- code_wars/century.py
+++ code_wars/century.py
@@ -10,3 +10,19 @@
 # 1900 --> 19
 # 1601 --> 17
 # 2000 --> 20
+
+
+
+# import codewars_test as test
+# from solution import century
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(century(1705), 18, 'Testing for year 1705')
+#         test.assert_equals(century(1900), 19, 'Testing for year 1900')
+#         test.assert_equals(century(1601), 17, 'Testing for year 1601')
+#         test.assert_equals(century(2000), 20, 'Testing for year 2000')
+#         test.assert_equals(century(356), 4, 'Testing for year 356')
+#         test.assert_equals(century(89), 1, 'Testing for year 89')
