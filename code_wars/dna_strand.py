daa7c987451be32096b9850433ec2e8e2319222c
tuyojr <olutuyod@gmail.com>
2022-12-20 08:37:54 +0100

    sample tests

--- code_wars/dna_strand.py
+++ code_wars/dna_strand.py
@@ -17,3 +17,17 @@
 
 # "ATTGC" --> "TAACG"
 # "GTAT" --> "CATA"
+
+
+
+
+# import codewars_test as test
+# from solution import DNA_strand
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():     
+#         test.assert_equals(DNA_strand("AAAA"),"TTTT","String AAAA is")
+#         test.assert_equals(DNA_strand("ATTGC"),"TAACG","String ATTGC is")
+#         test.assert_equals(DNA_strand("GTAT"),"CATA","String GTAT is")
