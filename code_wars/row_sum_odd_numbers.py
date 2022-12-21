6b6dba61f846f45073742556a689003efd0cb493
tuyojr <olutuyod@gmail.com>
2022-12-21 09:02:23 +0100

    sample tests

--- code_wars/row_sum_odd_numbers.py
+++ code_wars/row_sum_odd_numbers.py
@@ -11,3 +11,18 @@
 
 # 1 -->  1
 # 2 --> 3 + 5 = 8
+
+
+
+# import codewars_test as test
+# from solution import row_sum_odd_numbers
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(row_sum_odd_numbers(1), 1)
+#         test.assert_equals(row_sum_odd_numbers(2), 8)
+#         test.assert_equals(row_sum_odd_numbers(13), 2197)
+#         test.assert_equals(row_sum_odd_numbers(19), 6859)
+#         test.assert_equals(row_sum_odd_numbers(41), 68921)
