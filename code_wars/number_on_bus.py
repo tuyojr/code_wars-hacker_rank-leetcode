69037ac464bdcb372b4921f20cee599b81f2c238
tuyojr <olutuyod@gmail.com>
2022-12-21 12:02:02 +0100

    sample tests

--- code_wars/number_on_bus.py
+++ code_wars/number_on_bus.py
@@ -20,3 +20,16 @@
 
 # The second value in the first integer array is 0, since the 
 # bus is empty in the first bus stop.
+
+
+
+# import codewars_test as test
+# from solution import number
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(number([[10,0],[3,5],[5,8]]),5)
+#         test.assert_equals(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]),17)
+#         test.assert_equals(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]),21)
