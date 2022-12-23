f786ee83245d9be26a66cf3dda4bc6e486b75a73
tuyojr <olutuyod@gmail.com>
2022-12-23 09:51:17 +0100

    sample tests

--- code_wars/reverse_words.py
+++ code_wars/reverse_words.py
@@ -5,3 +5,17 @@
 # Examples
 # "This is an example!" ==> "sihT si na !elpmaxe"
 # "double  spaces"      ==> "elbuod  secaps"
+
+
+
+# import codewars_test as test
+# from solution import reverse_words
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(reverse_words('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god')
+#         test.assert_equals(reverse_words('apple'), 'elppa')
+#         test.assert_equals(reverse_words('a b c d'), 'a b c d')
+#         test.assert_equals(reverse_words('double  spaced  words'), 'elbuod  decaps  sdrow')
