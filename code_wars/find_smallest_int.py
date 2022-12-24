66ffe4fb5b3f90121f77b44a7a53e20c7b2cca61
tuyojr <olutuyod@gmail.com>
2022-12-24 08:00:51 +0100

    sample tests

--- code_wars/find_smallest_int.py
+++ code_wars/find_smallest_int.py
@@ -7,3 +7,21 @@
 # Given [34, -345, -1, 100] your solution will return -345
 # You can assume, for the purpose of this kata, that the supplied 
 # array will not be empty.
+
+
+
+# import codewars_test as test
+
+# # for backward compatibility
+# try:
+#     from solution import findSmallestInt as find_smallest_int
+# except ImportError:
+#     from solution import find_smallest_int
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(find_smallest_int([78, 56, 232, 12, 11, 43]), 11)
+#         test.assert_equals(find_smallest_int([78, 56, -2, 12, 8, -33]), -33)
+#         test.assert_equals(find_smallest_int([0, 1-2**64, 2**64]), 1-2**64)
