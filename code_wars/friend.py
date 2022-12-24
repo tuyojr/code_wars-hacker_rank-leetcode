ad844f5616eb59eb96520681a6d360150b889d5a
tuyojr <olutuyod@gmail.com>
2022-12-24 08:38:42 +0100

    sample tests

--- code_wars/friend.py
+++ code_wars/friend.py
@@ -10,3 +10,16 @@
 
 # friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
 # Note: keep the original order of the names in the output.
+
+
+
+# import codewars_test as test
+# from solution import friend
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Sample Test Cases')
+#     def sample_test_cases():
+#         test.assert_equals(friend(["Ryan", "Kieran", "Mark",]), ["Ryan", "Mark"])
+#         test.assert_equals(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"])
+#         test.assert_equals(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"])
