6ec84cef750d69cce01dc5a8d7d490c546dcd76d
tuyojr <olutuyod@gmail.com>
2022-12-25 07:17:34 +0100

    sample tests

--- code_wars/opposite.py
+++ code_wars/opposite.py
@@ -5,3 +5,19 @@
 # 1: -1
 # 14: -14
 # -34: 34
+
+
+
+# import codewars_test as test
+# from solution import opposite
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(opposite(1),-1)
+#         test.assert_equals(opposite(25.6), -25.6)
+#         test.assert_equals(opposite(0), 0)
+#         test.assert_equals(opposite(1425.2222), -1425.2222)
+#         test.assert_equals(opposite(-3.1458), 3.1458)
+#         test.assert_equals(opposite(-95858588225),95858588225)
