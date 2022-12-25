e6f69cd3f0eb04d80fb75ac2f517b27dc988c113
tuyojr <olutuyod@gmail.com>
2022-12-25 07:04:24 +0100

    sample tests

--- code_wars/past.py
+++ code_wars/past.py
@@ -14,3 +14,18 @@
 # 0 <= h <= 23
 # 0 <= m <= 59
 # 0 <= s <= 59
+
+
+
+# import codewars_test as test
+# from solution import past
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(past(0,1,1),61000)
+#         test.assert_equals(past(1,1,1),3661000)
+#         test.assert_equals(past(0,0,0),0)
+#         test.assert_equals(past(1,0,1),3601000)
+#         test.assert_equals(past(1,0,0),3600000)
