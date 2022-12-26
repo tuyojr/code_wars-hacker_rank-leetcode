5b2261e27b5fc3f43ac452bc6b26ff030c639c2f
tuyojr <olutuyod@gmail.com>
2022-12-26 13:54:17 +0100

    sample tests

--- code_wars/count_positives_sum_negatives.py
+++ code_wars/count_positives_sum_negatives.py
@@ -9,3 +9,19 @@
 # Example
 # For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], 
 # you should return [10, -65].
+
+
+
+# import codewars_test as test
+# from solution import count_positives_sum_negatives
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65])
+#         test.assert_equals(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]),[8,-50])
+#         test.assert_equals(count_positives_sum_negatives([1]),[1,0])
+#         test.assert_equals(count_positives_sum_negatives([-1]),[0,-1])
+#         test.assert_equals(count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]),[0,0])
+#         test.assert_equals(count_positives_sum_negatives([]),[])
