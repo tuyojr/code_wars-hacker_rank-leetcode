9d155676241ca381e37bd5a23b418e49d761bfc1
tuyojr <olutuyod@gmail.com>
2022-12-26 13:20:59 +0100

    sample tests

--- code_wars/nb_year.py
+++ code_wars/nb_year.py
@@ -29,3 +29,17 @@
 # Don't forget to convert the percent parameter as a percentage in the 
 # body of your function: if the parameter percent is 2 you have to 
 # convert it to 0.02.
+
+
+
+# import codewars_test as test
+# from solution import nb_year
+
+# @test.describe("nb_year")
+# def tests():
+#     @test.it("basic tests")
+#     def basics():
+#         test.assert_equals(nb_year(1500, 5, 100, 5000), 15)
+#         test.assert_equals(nb_year(1500000, 2.5, 10000, 2000000), 10)
+#         test.assert_equals(nb_year(1500000, 0.25, 1000, 2000000), 94)
+        
