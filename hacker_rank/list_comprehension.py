18a8e5208dcff8e0b81421c244ecc802867a302e
tuyojr <olutuyod@gmail.com>
2022-12-27 08:21:19 +0100

    list comprehension solution

--- hacker_rank/list_comprehension.py
+++ hacker_rank/list_comprehension.py
@@ -59,3 +59,14 @@
 # [1, 0, 0], [1, 0, 2], [1, 1, 1], [1, 1, 2], [1, 2, 0], [1, 2, 1], 
 # [1, 2, 2], [2, 0, 1], [2, 0, 2], [2, 1, 0], [2, 1, 1], [2, 1, 2], 
 # [2, 2, 0], [2, 2, 1], [2, 2, 2]]
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    # list() creates the eventual list we need
+    # in that list, we have a list of three integers i, j, k.
+    # each loop maps to each integer, and we use the input as our range
+    # at the end, we specify the required condition to get our desired list
+    print(list([i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)  if i+j+k !=n))
