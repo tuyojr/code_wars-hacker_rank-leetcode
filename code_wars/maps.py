033d411ec4c7c7db6b739839b2aa15a760a9836d
tuyojr <olutuyod@gmail.com>
2022-12-27 11:34:50 +0100

    sample tests

--- code_wars/maps.py
+++ code_wars/maps.py
@@ -2,4 +2,17 @@
 
 # For example:
 
-# [1, 2, 3] --> [2, 4, 6]s
+# [1, 2, 3] --> [2, 4, 6]
+
+
+
+# import codewars_test as test
+# from solution import maps
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(maps([1, 2, 3]), [2, 4, 6])
+#         test.assert_equals(maps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), [0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+#         test.assert_equals(maps([]), [])
