546f3685f19cb9fee60ec83da83146fe2661655d
tuyojr <olutuyod@gmail.com>
2022-12-27 12:31:05 +0100

    nested lists solution

--- hacker_rank/nested_lists.py
+++ hacker_rank/nested_lists.py
@@ -56,3 +56,35 @@
 # The lowest grade of 37.2 belongs to Tina. The second lowest grade of 37.21
 # belongs to both Harry and Berry, so we order their names alphabetically 
 # and print each name on a new line.
+
+if __name__ == '__main__':
+    N = int(input())
+    students = []
+
+    for i in range(N):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+    
+    # reverse = None (Sorts in Ascending order)
+    # key is set to sort using second element of 
+    # sublist lambda has been used
+    students.sort(key = lambda x: x[1])
+    
+    # list comprehension that removes the first lowest score from the original list
+    # loops through each list in the students list and if the first score is not equal to the 
+    # score of the second student, add it to this new list a
+    a = [ i for i in students if i[1] != students[0][1]]
+
+    # list comprehension to create a list of students with the same score.
+    # this is created by looping through the initial list, a, above. if the
+    # next student in the list has the same score as that in the previous student,
+    # add it to this new list
+    b = [ i for i in a if i[1] == a[0][1]]
+
+    # this sorts the names in the nested list in an alphabetical order
+    b.sort(key = lambda x: x[0])
+
+    # loop through the length of final list and extract only the name 
+    for i in range (len(b)):
+        print(b[i][0])    
