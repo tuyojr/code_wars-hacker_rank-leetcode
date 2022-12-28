83700e0cb173fe6ad60195a6293484e802d3627e
tuyojr <olutuyod@gmail.com>
2022-12-28 07:54:21 +0100

    sample test

--- code_wars/to_jaden_case.py
+++ code_wars/to_jaden_case.py
@@ -16,3 +16,16 @@
 
 # Link to Jaden's former Twitter account @officialjaden via archive.org
 # https://web.archive.org/web/20190624190255/https://twitter.com/officialjaden
+
+
+
+# from solution import to_jaden_case
+# import codewars_test as test
+
+
+# @test.describe('Sample test')
+# def basic_tests():
+#     @test.it('Simple text')
+#     def _():
+#         quote = "How can mirrors be real if our eyes aren't real"
+#         test.assert_equals(to_jaden_case(quote), "How Can Mirrors Be Real If Our Eyes Aren't Real")
