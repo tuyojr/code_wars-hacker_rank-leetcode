1603aa9357dd093e117fdd0edf685ffbafae86b8
tuyojr <olutuyod@gmail.com>
2022-12-29 08:15:40 +0100

    sample tests

--- code_wars/smash.py
+++ code_wars/smash.py
@@ -7,3 +7,24 @@
 
 # Example
 # ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+
+
+# import codewars_test as test
+# from solution import smash
+
+# @test.describe("smash")
+# def _():
+#     @test.it("Should return empty string for empty array.")
+#     def _():
+#         test.assert_equals(smash([]), "")
+        
+#     @test.it("One word example should return the word.")
+#     def _():
+#         test.assert_equals(smash(["hello"]), "hello")
+        
+#     @test.it("Multiple words should be separated by spaces.")
+#     def _():
+#         test.assert_equals(smash(["hello", "world"]), "hello world")
+#         test.assert_equals(smash(["hello", "amazing", "world"]), "hello amazing world")
+#         test.assert_equals(smash(["this", "is", "a", "really", "long", "sentence"]), "this is a really long sentence")
