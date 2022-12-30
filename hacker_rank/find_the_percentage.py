8b7e64350cc4ce5d32e897c2a489599612c44d2f
tuyojr <olutuyod@gmail.com>
2022-12-30 09:07:41 +0100

    finding the percentage solution

--- hacker_rank/find_the_percentage.py
+++ hacker_rank/find_the_percentage.py
@@ -50,3 +50,17 @@
 
 # Sample Output 1
 # 26.50
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    if query_name in student_marks:
+        values = student_marks.get(query_name)
+        total = sum(values)
+        average = total / len(values)
+        print(format(average, '.2f'))
