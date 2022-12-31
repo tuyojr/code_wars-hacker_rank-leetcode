44173e1719776e5844ddfb1e27832fb60bcea403
tuyojr <olutuyod@gmail.com>
2023-01-01 00:46:29 +0100

    sample tests

--- code_wars/count_sheeps.py
+++ code_wars/count_sheeps.py
@@ -13,3 +13,23 @@
 # The correct answer would be 17.
 
 # Hint: Don't forget to check for bad values like null/undefined
+
+
+
+# import codewars_test as test
+# from solution import count_sheeps
+
+# @test.describe("Basic Tests")
+# def basic_tests():
+    
+#     @test.it("Basic Tests")
+#     def basic_tests():
+#         array1 = [True,  True,  True,  False,
+#                   True,  True,  True,  True ,
+#                   True,  False, True,  False,
+#                   True,  False, False, True ,
+#                   True,  True,  True,  True ,
+#                   False, False, True,  True ];
+        
+#         result = count_sheeps(array1)
+#         test.assert_equals(result, 17, f"There are 17 sheeps in total, not {result}")
