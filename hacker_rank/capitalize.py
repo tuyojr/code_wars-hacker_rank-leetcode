bf391a00221f40dd80222aa9f9f9c66482f210e2
tuyojr <olutuyod@gmail.com>
2022-12-31 08:07:21 +0100

    capitalize problem solution

--- hacker_rank/capitalize.py
+++ hacker_rank/capitalize.py
@@ -22,3 +22,35 @@
 
 # Sample Output
 # Chris Alan
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    
+    # create a variable to hold each name
+    full_name = s.split(" ")
+    
+    # use a list comprehension to capitalize the first letter of each name
+    capitalized = [name.capitalize() for name in full_name]
+    
+    # use the .join method to return the full name in proper case
+    return " ".join(capitalized)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
