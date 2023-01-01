b762851ff91efa02d22bb8fa830c31128322b02a
tuyojr <olutuyod@gmail.com>
2023-01-01 01:34:20 +0100

    sample tests

--- code_wars/grow.py
+++ code_wars/grow.py
@@ -2,3 +2,21 @@
 # multiplying the values together in order. Example:
 
 # [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+
+# import codewars_test as test
+# from solution import grow
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         tests = [
+#             [6 , [1, 2, 3]],
+#             [16, [4, 1, 1, 1, 4]],
+#             [64, [2, 2, 2, 2, 2, 2]],
+#         ]
+        
+#         for exp, inp in tests:
+#             test.assert_equals(grow(inp), exp)
