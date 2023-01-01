342e2f097dd1a5d6498c63d78042c43aba37d376
tuyojr <olutuyod@gmail.com>
2023-01-01 01:29:08 +0100

    fake binary kata solution

--- code_wars/fake_bin.py
+++ code_wars/fake_bin.py
@@ -3,3 +3,33 @@
 # resulting string.
 
 # Note: input will never be an empty string
+
+def fake_bin(x):
+    bin = []
+    for i in x:
+        bin.append(i)
+    for j, item in enumerate(bin):
+        if (int(item) < 5):
+            bin[j] = '0'
+        else:
+            bin[j] = '1'
+    return ''.join(bin)
+    # return ''.join('0' if c < '5' else '1' for c in x)
+# import codewars_test as test
+# from solution import fake_bin
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         tests = [
+#             # [expected, input]
+#             ["01011110001100111", "45385593107843568"],
+#             ["101000111101101", "509321967506747"],
+#             ["011011110000101010000011011", "366058562030849490134388085"],
+#             ["01111100", "15889923"],
+#             ["100111001111", "800857237867"],
+#         ]
+        
+#         for exp, inp in tests:
+#             test.assert_equals(fake_bin(inp), exp)
