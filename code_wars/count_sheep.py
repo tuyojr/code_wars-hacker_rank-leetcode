07f562f7063ecbc849461e5ec06b0fb03b08bc51
tuyojr <olutuyod@gmail.com>
2023-01-01 01:41:58 +0100

    sample tests

--- code_wars/count_sheep.py
+++ code_wars/count_sheep.py
@@ -1,3 +1,17 @@
 # Given a non-negative integer, 3 for example, return a 
 # string with a murmur: "1 sheep...2 sheep...3 sheep...". 
 # Input will always be valid, i.e. no negative integers.
+
+
+
+# import codewars_test as test
+# from solution import count_sheep
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(count_sheep(0), "");
+#         test.assert_equals(count_sheep(1), "1 sheep...");
+#         test.assert_equals(count_sheep(2), "1 sheep...2 sheep...")
+#         test.assert_equals(count_sheep(3), "1 sheep...2 sheep...3 sheep...")
