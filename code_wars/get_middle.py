1ce9204d2a88bded48b1a7f33bd437d4a3f80a42
tuyojr <olutuyod@gmail.com>
2023-01-03 08:18:11 +0100

    sample tests

--- code_wars/get_middle.py
+++ code_wars/get_middle.py
@@ -22,3 +22,19 @@
 # #Output
 
 # The middle character(s) of the word represented as a string.
+
+
+
+# import codewars_test as test
+# from solution import get_middle
+
+# @test.describe("Basic Tests")
+# def basic_tests():
+    
+#     @test.it("Basic Tests")
+#     def basic_tests():
+#         test.assert_equals(get_middle("test"),"es")
+#         test.assert_equals(get_middle("testing"),"t")
+#         test.assert_equals(get_middle("middle"),"dd")
+#         test.assert_equals(get_middle("A"),"A")
+#         test.assert_equals(get_middle("of"),"of")
