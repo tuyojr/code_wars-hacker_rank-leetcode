1b78124b8290293335deec420f3caf0671f1905a
tuyojr <olutuyod@gmail.com>
2023-01-04 10:45:28 +0100

    tuples problem solution

--- hacker_rank/tuples.py
+++ hacker_rank/tuples.py
@@ -21,3 +21,11 @@
 
 # Sample Output 0
 # 3713081631934410656
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Pypy3
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    t = tuple(integer_list)
+    print(hash(t))
