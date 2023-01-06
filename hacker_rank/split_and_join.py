3f749a04ca1117b08f8f13e8dba2d07d1f24bd41
tuyojr <olutuyod@gmail.com>
2023-01-06 07:24:51 +0100

    string split and join problem solution

--- hacker_rank/split_and_join.py
+++ hacker_rank/split_and_join.py
@@ -34,3 +34,12 @@
 
 # Sample Output
 # this-is-a-string
+
+def split_and_join(line):
+    # write your code here
+    return "-".join(line.split())
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
