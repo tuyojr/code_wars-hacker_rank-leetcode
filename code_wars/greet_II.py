abf74fca4a3fa690e47a37d34172cdac7d4cfa47
tuyojr <olutuyod@gmail.com>
2023-01-07 20:58:59 +0100

    sample tests

--- code_wars/greet_II.py
+++ code_wars/greet_II.py
@@ -6,3 +6,15 @@
 # case	                 return
 # name equals owner	    'Hello boss'
 # otherwise	            'Hello guest'
+
+
+
+# import codewars_test as test
+# from solution import greet
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(greet('Daniel', 'Daniel'), 'Hello boss')
+#         test.assert_equals(greet('Greg', 'Daniel'), 'Hello guest')
