34385d7ddf7299e31d4f52245b13b5673bb52cee
tuyojr <olutuyod@gmail.com>
2023-01-07 21:15:22 +0100

    string validator problem solution

--- hacker_rank/string_validators.py
+++ hacker_rank/string_validators.py
@@ -69,3 +69,37 @@
 # True
 # True
 # True
+
+if __name__ == '__main__':
+    s = input()
+    def alphanum(s):
+        for i in s:
+            if i.isalnum():
+                return True
+        return False
+    print(alphanum(s))
+
+    def alpha(s):
+        for i in s:
+            if i.isalpha():
+                return True
+        return False
+    print(alpha(s))
+    def digit(s):
+        for i in s:
+            if i.isdigit():
+                return True
+        return False
+    print(digit(s))
+    def lower(s):
+        for i in s:
+            if i.islower():
+                return True
+        return False
+    print(lower(s))
+    def upper(s):
+        for i in s:
+            if i.isupper():
+                return True
+        return False
+    print(upper(s))
