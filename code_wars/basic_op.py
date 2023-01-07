cc10ebdb63dbf8bdb73f30024e6cf9d9c2138584
tuyojr <olutuyod@gmail.com>
2023-01-07 07:25:48 +0100

    sample tests

--- code_wars/basic_op.py
+++ code_wars/basic_op.py
@@ -11,3 +11,17 @@
 # ('-', 15, 18) --> -3
 # ('*', 5, 5) --> 25
 # ('/', 49, 7) --> 7
+
+
+
+# import codewars_test as test
+# from solution import basic_op
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(basic_op('+', 4, 7), 11)
+#         test.assert_equals(basic_op('-', 15, 18), -3)
+#         test.assert_equals(basic_op('*', 5, 5), 25)
+#         test.assert_equals(basic_op('/', 49, 7), 7)
