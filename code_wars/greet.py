9552d3215442d4aeaaed1cb05e3ad288f904c3d0
tuyojr <olutuyod@gmail.com>
2023-01-07 07:22:17 +0100

    sample tests

--- code_wars/greet.py
+++ code_wars/greet.py
@@ -8,17 +8,17 @@
 
 
 
-import codewars_test as test
-from solution import greet
+# import codewars_test as test
+# from solution import greet
 
-@test.describe("Greet function")
-def _():
-    @test.it("Making sure greet exists")
-    def _():
-        try:
-            test.expect(greet)
-        except NameError:
-            test.fail("Greet doesn't exist")
-    @test.it("Testing that it returns hello world!")
-    def _():
-        test.assert_equals(greet(), "hello world!", "Greet doesn't return hello world!")
+# @test.describe("Greet function")
+# def _():
+#     @test.it("Making sure greet exists")
+#     def _():
+#         try:
+#             test.expect(greet)
+#         except NameError:
+#             test.fail("Greet doesn't exist")
+#     @test.it("Testing that it returns hello world!")
+#     def _():
+#         test.assert_equals(greet(), "hello world!", "Greet doesn't return hello world!")
