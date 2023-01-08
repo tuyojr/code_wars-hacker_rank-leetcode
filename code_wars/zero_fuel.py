f7254fc4d156bdb9fd66f73c974eb9247d20e45c
tuyojr <olutuyod@gmail.com>
2023-01-08 08:37:48 +0100

    sample tests

--- code_wars/zero_fuel.py
+++ code_wars/zero_fuel.py
@@ -8,3 +8,19 @@
 # is possible to get to the pump or not.
 
 # Function should return true if it is possible and false if not.
+
+
+
+# import codewars_test as test
+
+# try: 
+#     from solution import zeroFuel as zero_fuel
+# except ImportError: 
+#     from solution import zero_fuel 
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(zero_fuel(50, 25, 2), True)
+#         test.assert_equals(zero_fuel(100, 50, 1), False)
