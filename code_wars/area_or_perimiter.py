7831190ecb98b2a09edeb0d355e836c856fc581b
tuyojr <olutuyod@gmail.com>
2023-01-08 09:00:43 +0100

    area or perimeter kata solution

--- code_wars/area_or_perimiter.py
+++ code_wars/area_or_perimiter.py
@@ -9,3 +9,24 @@
 
 # Note: for the purposes of this kata you will assume that it is a square if 
 # its length and width are equal, otherwise it is a rectangle.
+
+def area_or_perimeter(l , w):
+    # return your answer
+    if (l == w):
+
+        # area of a square = l * w
+        return l * w
+    else:
+
+        # perimeter of a rectangle = 2(l * w)
+        return 2 * (l + w)
+
+# import codewars_test as test
+# from solution import area_or_perimeter
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(area_or_perimeter(4, 4), 16)
+#         test.assert_equals(area_or_perimeter(6, 10), 32)
