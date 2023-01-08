0ba1a0a4cee3de7897833f0215aa51cd34a4d24a
tuyojr <olutuyod@gmail.com>
2023-01-08 07:37:08 +0100

    text alignment problem description

--- hacker_rank/text_alignment.py
+++ hacker_rank/text_alignment.py
@@ -35,3 +35,30 @@
 
 # Sample Input
 # 5
+
+# Sample Output
+#     H    
+#    HHH   
+#   HHHHH  
+#  HHHHHHH 
+# HHHHHHHHH
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#                     HHHHHHHHH 
+#                      HHHHHHH  
+#                       HHHHH   
+#                        HHH    
+#                         H 
