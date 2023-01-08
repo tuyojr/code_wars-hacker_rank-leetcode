c8a9b92f78555cf8f18831f1af916b59a8b16a77
tuyojr <olutuyod@gmail.com>
2023-01-08 08:33:43 +0100

    text wrap problem solution

--- hacker_rank/wrap.py
+++ hacker_rank/wrap.py
@@ -33,3 +33,13 @@
 # QRST
 # UVWX
 # YZ
+
+import textwrap
+
+def wrap(string, max_width):
+    return '\n'.join(textwrap.wrap(string, max_width))
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
