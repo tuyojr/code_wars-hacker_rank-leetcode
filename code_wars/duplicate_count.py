a28fb8669f5af4fa6d10b08d1738ba66d4b4e6c2
tuyojr <olutuyod@gmail.com>
2023-01-09 07:15:52 +0100

    sample tests

--- code_wars/duplicate_count.py
+++ code_wars/duplicate_count.py
@@ -12,3 +12,18 @@
 # "Indivisibilities" -> 2 ## 'i' occurs seven times and 's' occurs twice
 # "aA11" -> 2 ## 'a' and '1'
 # "ABBA" -> 2 ## 'A' and 'B' each occur twice
+
+
+
+# import codewars_test as test
+# from solution import duplicate_count
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it("Basic Tests")
+#     def basic_tests():
+#         test.assert_equals(duplicate_count(""), 0)
+#         test.assert_equals(duplicate_count("abcde"), 0)
+#         test.assert_equals(duplicate_count("abcdeaa"), 1)
+#         test.assert_equals(duplicate_count("abcdeaB"), 2)
+#         test.assert_equals(duplicate_count("Indivisibilities"), 2)
