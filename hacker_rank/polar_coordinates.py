7deff4f7607ad2b1a6061934a4a92cface48b0e3
tuyojr <olutuyod@gmail.com>
2023-01-15 08:37:09 +0100

    polar coordinates challenge solution

--- hacker_rank/polar_coordinates.py
+++ hacker_rank/polar_coordinates.py
@@ -1,1 +1,7 @@
 # https://www.hackerrank.com/challenges/polar-coordinates/problem?isFullScreen=true
+
+import cmath
+if __name__ == '__main__':
+    z = complex(input())
+    print(abs(z))
+    print(cmath.phase(z))
