d8a5d2db3c6e843c3e78303eb3378f556b0cb45f
tuyojr <olutuyod@gmail.com>
2023-01-17 06:34:22 +0100

    time delta challenge solution

--- hacker_rank/time_delta.py
+++ hacker_rank/time_delta.py
@@ -50,3 +50,43 @@
 # Similarly, in the second query, time difference is 5 hours and 
 # 30 minutes for time zone adjusting for that we have a difference 
 # of 1 day and 30 minutes. Or 24 x 3600 + 30 x 60 ===> 88200.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime
+from datetime import timedelta
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    
+    #convert string into datetime object with datetime.strptime(date_time_string_input, format input)
+    date1 = datetime.strptime(t1,"%a %d %b %Y %H:%M:%S %z")
+    date2 = datetime.strptime(t2,"%a %d %b %Y %H:%M:%S %z")
+    
+    #calculate timedelta
+    timedelta = date1 - date2
+    
+    #convert timedelta to total_seconds
+    timedelta = abs(timedelta.total_seconds())
+    
+    #format string number of total second
+    tdelta = ("{:.0f}".format(timedelta))
+    
+    return tdelta
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+        t2 = input()
+        delta = time_delta(t1, t2)
+        fptr.write(delta + '\n')
+    fptr.close()
