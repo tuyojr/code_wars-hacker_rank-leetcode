e380b688de2050ac2fac63346168111722071169
tuyojr <olutuyod@gmail.com>
2023-01-18 08:59:45 +0100

    sample tests

--- code_wars/even_or_odd.py
+++ code_wars/even_or_odd.py
@@ -1,2 +1,23 @@
 # Create a function that takes an integer as an argument and 
 # returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+# import codewars_test as test
+# from solution import even_or_odd
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(even_or_odd(2), "Even")
+#         test.assert_equals(even_or_odd(1), "Odd")
+#         test.assert_equals(even_or_odd(0), "Even")
+#         test.assert_equals(even_or_odd(1545452), "Even")
+#         test.assert_equals(even_or_odd(7), "Odd")
+#         test.assert_equals(even_or_odd(78), "Even")
+#         test.assert_equals(even_or_odd(17), "Odd")
+#         test.assert_equals(even_or_odd(74156741), "Odd")
+#         test.assert_equals(even_or_odd(100000), "Even")
+#         test.assert_equals(even_or_odd(-123), "Odd")
+#         test.assert_equals(even_or_odd(-456), "Even")
