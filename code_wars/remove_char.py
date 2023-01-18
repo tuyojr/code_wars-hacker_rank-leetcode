d1bd16f4d0688cf552c5d4593f4bc66f0c05bf2b
tuyojr <olutuyod@gmail.com>
2023-01-18 08:39:38 +0100

    sample tests

--- code_wars/remove_char.py
+++ code_wars/remove_char.py
@@ -2,3 +2,19 @@
 # that removes the first and last characters of a string. You're 
 # given one parameter, the original string. You don't have to 
 # worry with strings with less than two characters.
+
+
+
+# import codewars_test as test
+# from solution import remove_char
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(remove_char('eloquent'), 'loquen')
+#         test.assert_equals(remove_char('country'), 'ountr')
+#         test.assert_equals(remove_char('person'), 'erso')
+#         test.assert_equals(remove_char('place'), 'lac')
+#         test.assert_equals(remove_char('ok'), '')
+#         test.assert_equals(remove_char('ooopsss'), 'oopss')
