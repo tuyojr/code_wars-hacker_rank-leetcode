5457a67e83f9c0162f011c3c83e740d75e375582
tuyojr <olutuyod@gmail.com>
2023-01-18 08:34:15 +0100

    no idea! challenge solution

--- hacker_rank/no_idea.py
+++ hacker_rank/no_idea.py
@@ -43,3 +43,32 @@
 # in the array so it is not included in the calculation.
 
 # Hence, the total happiness is 2 - 1 = 1.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == '__main__':
+    
+    # Prompt the user for the amount of elements to be in n and m arr
+    n, m = input().split()
+    
+    # create an array for the n integers
+    arr = input().split()
+    
+    # prompt the user for the m integers A and B
+    A = set(input().split())
+    B = set(input().split())
+
+    # create a variable to hold the happiness
+    happiness = 0
+
+    # loop through the array
+    for i in arr:
+        
+        # Increment the happiness for items in A which we like, and decrement them for items in B which we dislike
+        if i in A:
+            happiness += 1
+        elif i in B:
+            happiness -= 1
+
+    # output the happiness
+    print(happiness)
