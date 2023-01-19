7b2eaa0dcd304468bd82f3d9b6677485ebe52557
tuyojr <olutuyod@gmail.com>
2023-01-19 07:26:30 +0100

    sample tests

--- code_wars/get_count.py
+++ code_wars/get_count.py
@@ -3,3 +3,31 @@
 # We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 # The input string will only consist of lower case letters and/or spaces.
+
+
+
+# import codewars_test as test
+# from solution import get_count
+
+# @test.describe("Sample tests")
+# def sample_tests():
+    
+#     @test.it("Should count all vowels")
+#     def all_vowels():
+#         test.assert_equals(get_count("aeiou"), 5, f"Incorrect answer for \"aeiou\"")
+        
+#     @test.it("Should not count \"y\"")
+#     def only_y():
+#         test.assert_equals(get_count("y"), 0, f"Incorrect answer for \"y\"")        
+        
+#     @test.it("Should return 0 when no vowels")
+#     def no_vowels():
+#         test.assert_equals(get_count("bcdfghjklmnpqrstvwxz y"), 0, f"Incorrect answer for \"bcdfghjklmnpqrstvwxz y\"")
+        
+#     @test.it("Should return 0 for empty string")
+#     def no_vowels():
+#         test.assert_equals(get_count(""), 0, f"Incorrect answer for empty string")
+        
+#     @test.it("Should return 5 for \"abracadabra\"")
+#     def test_abracadabra():    
+#         test.assert_equals(get_count("abracadabra"), 5, f"Incorrect answer for \"abracadabra\"")
