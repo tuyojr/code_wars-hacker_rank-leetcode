d1cc81e5ef71515b4ebda5e9a3513feef40e9b6b
tuyojr <olutuyod@gmail.com>
2023-01-21 08:04:35 +0100

    grasshopper - summation 6kyu kata solution

--- code_wars/summation.py
+++ code_wars/summation.py
@@ -8,7 +8,8 @@
 # 2 -> 3 (1 + 2)
 # 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
-
+def summation(num):
+    return sum(range(1, num + 1))
 
 # import codewars_test as test
 # from solution import summation
