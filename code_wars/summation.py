9df72392a5bd949934d5bfca817076745ded9e33
tuyojr <olutuyod@gmail.com>
2023-01-21 08:02:00 +0100

    sample tests

--- code_wars/summation.py
+++ code_wars/summation.py
@@ -7,3 +7,18 @@
 
 # 2 -> 3 (1 + 2)
 # 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+
+
+# import codewars_test as test
+# from solution import summation
+
+# @test.describe('Fixed tests')
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(summation(1), 1)
+#         test.assert_equals(summation(8), 36)
+#         test.assert_equals(summation(22), 253)
+#         test.assert_equals(summation(100), 5050)
+#         test.assert_equals(summation(213), 22791)
