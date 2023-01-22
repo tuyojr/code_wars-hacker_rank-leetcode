c12fd3be92f070163051c6b113574eaa8582916e
tuyojr <olutuyod@gmail.com>
2023-01-22 07:36:25 +0100

    sample tests

--- code_wars/square_sum.py
+++ code_wars/square_sum.py
@@ -2,3 +2,18 @@
 # passed into it and then sums the results together.
 
 # For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+
+# import codewars_test as test
+# from solution import square_sum
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(square_sum([1,2]), 5)
+#         test.assert_equals(square_sum([0, 3, 4, 5]), 50)
+#         test.assert_equals(square_sum([]), 0)
+#         test.assert_equals(square_sum([-1,-2]), 5)
+#         test.assert_equals(square_sum([-1,0,1]), 2)
