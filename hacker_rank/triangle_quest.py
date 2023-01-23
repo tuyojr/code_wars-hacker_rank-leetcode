388b24b97a5feb6bdbecfd5e1d4b57043f8e5778
tuyojr <olutuyod@gmail.com>
2023-01-23 08:32:29 +0100

    triangle quest 2 challenge solution

--- hacker_rank/triangle_quest.py
+++ hacker_rank/triangle_quest.py
@@ -38,3 +38,11 @@
 # 12321
 # 1234321
 # 123454321
+
+    # i starts from 1, the first iterations outputs 1 ---> 10**1-1 == 9, 9//9 == 1, 1**2 == 1
+    # the second iteration outputs 121 ---> 10**2-1 == 99, 99//9 == 11, 11**2 == 121
+    # the third iteration outputs 12321 ---> 10**3-1 == 999, 999//9 == 111, 111**2 == 12321
+    # the fourth iteration outputs 1234321 ---> 10**4-1 == 9999, 9999//9 == 1111, 1111**2 == 1234321
+    # the fifth iteration outputs 123454321 ---> 10**5-1 == 99999, 99999//9 == 11111, 11111**2 == 123454321
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i-1)//9)**2)
