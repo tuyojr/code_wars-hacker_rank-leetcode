fbd20f4182d399d0a7eade9eb29b2f6a53eae5c7
tuyojr <olutuyod@gmail.com>
2023-01-24 08:18:08 +0100

    sample tests

--- code_wars/is_pangram.py
+++ code_wars/is_pangram.py
@@ -6,3 +6,19 @@
 
 # Given a string, detect whether or not it is a pangram. Return 
 # True if it is, False if not. Ignore numbers and punctuation.
+
+
+
+# from solution import is_pangram
+# import codewars_test as test
+
+# @test.describe("sample tests")
+# def sample_tests():
+    
+#     @test.it("Should return true for a pangram")
+#     def test_pangram():        
+#         test.assert_equals(is_pangram("The quick, brown fox jumps over the lazy dog!"), True)
+
+#     @test.it("Should return false for not a pangram")
+#     def test_not_pangram():        
+#         test.assert_equals(is_pangram("1bcdefghijklmnopqrstuvwxyz"), False)
