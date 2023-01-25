2be7d1cf90571aa36a483c4552e878084bf7ac97
tuyojr <olutuyod@gmail.com>
2023-01-25 08:51:07 +0100

    power - mod power challenge solutiob

--- hacker_rank/power.py
+++ hacker_rank/power.py
@@ -44,3 +44,12 @@
 
 # 81
 # 1
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    print(pow(a, b))
+    print(pow(a, b, m))
