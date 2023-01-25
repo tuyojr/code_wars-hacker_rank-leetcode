d2520580dd71984e7180b11073b7a2ee18818797
tuyojr <olutuyod@gmail.com>
2023-01-25 08:56:53 +0100

    sample tests

--- code_wars/sum_dig_pow.py
+++ code_wars/sum_dig_pow.py
@@ -26,3 +26,19 @@
 # 90, 100 --> []
 
 # Enjoy it!!
+
+
+
+# from solution import sum_dig_pow
+# import codewars_test as test
+
+# @test.describe("Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!")
+# def desc1():
+#     @test.it('Sample tests')
+#     def it1():
+#         test.assert_equals( sum_dig_pow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+#         test.assert_equals(sum_dig_pow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89])
+#         test.assert_equals(sum_dig_pow(10, 89),  [89])
+#         test.assert_equals(sum_dig_pow(10, 100),  [89])
+#         test.assert_equals(sum_dig_pow(90, 100), [])
+#         test.assert_equals(sum_dig_pow(89, 135), [89, 135])
