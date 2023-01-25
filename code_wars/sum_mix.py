7c3ce242a77fc234366d3f71b5ff8e44b40d9d20
tuyojr <olutuyod@gmail.com>
2023-01-25 09:22:17 +0100

    sample tests

--- code_wars/sum_mix.py
+++ code_wars/sum_mix.py
@@ -2,3 +2,18 @@
 # the sum of the array values as if all were numbers.
 
 # Return your answer as a number.
+
+
+
+# import codewars_test as test
+# from solution import sum_mix
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(sum_mix([9, 3, '7', '3']), 22)
+#         test.assert_equals(sum_mix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42)
+#         test.assert_equals(sum_mix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41)
+#         test.assert_equals(sum_mix(['1', '5', '8', 8, 9, 9, 2, '3']), 45)
+#         test.assert_equals(sum_mix([8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7]), 61)
