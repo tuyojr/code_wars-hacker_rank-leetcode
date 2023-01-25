b7267a81833ed6a591c2690a1bb785b16be5a2cd
tuyojr <olutuyod@gmail.com>
2023-01-25 09:18:51 +0100

    remomving elements 6kyu kata solution

--- code_wars/remove_every_other.py
+++ code_wars/remove_every_other.py
@@ -6,7 +6,22 @@
 
 # None of the arrays will be empty, so you don't have to worry about that!
 
+def remove_every_other(my_list):
+    # Your code here!
+    
+    # create a list to hold the elements that meet the criteria
+    new_list = []
 
+    # loop through the list
+    for i in range(len(my_list)):
+        # if the index is even, add the element to the new list
+        if i % 2 == 0:
+            new_list.append(my_list[i])
+        
+    return new_list
+
+    # # one line solution
+    # return my_list[::2]
 
 # test.assert_equals(remove_every_other(['Hello', 'Goodbye', 'Hello Again']),
 #                    ['Hello', 'Hello Again'])
