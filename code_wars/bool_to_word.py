0dfa6ee105af4c8b8298a8844e9a6498824f7cdc
tuyojr <olutuyod@gmail.com>
2023-01-25 09:31:14 +0100

    bool conversion to string 6kyu kata solution

--- code_wars/bool_to_word.py
+++ code_wars/bool_to_word.py
@@ -1,7 +1,12 @@
 # Complete the method that takes a boolean value and return 
 # a "Yes" string for true, or a "No" string for false.
 
-
+def bool_to_word(boolean):
+    # TODO
+    if (boolean == True):
+        return "Yes"
+    elif (boolean == False):
+        return "No"
 
 # import codewars_test as test
 # from solution import bool_to_word
