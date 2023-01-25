84a252b1bca9dc3d0030fe9e7422154724589cee
tuyojr <olutuyod@gmail.com>
2023-01-25 09:26:26 +0100

    volume of cuboid 6kyu kata solution

--- code_wars/get_volume_of_cuboid.py
+++ code_wars/get_volume_of_cuboid.py
@@ -2,7 +2,9 @@
 # with three values: the length, width and height of the cuboid. 
 # Write a function to help Bob with this calculation.
 
-
+def get_volume_of_cuboid(length, width, height):
+    # Code goes here...
+    return length * width * height
 
 # import codewars_test as test
 # from solution import get_volume_of_cuboid
