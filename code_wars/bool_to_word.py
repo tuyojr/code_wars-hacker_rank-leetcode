fc310df5c1d894f99bb5a36e4f661a239893bbcf
tuyojr <olutuyod@gmail.com>
2023-01-25 09:31:00 +0100

    sample tests

--- code_wars/bool_to_word.py
+++ code_wars/bool_to_word.py
@@ -1,2 +1,14 @@
 # Complete the method that takes a boolean value and return 
 # a "Yes" string for true, or a "No" string for false.
+
+
+
+# import codewars_test as test
+# from solution import bool_to_word
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(bool_to_word(True), 'Yes')
+#         test.assert_equals(bool_to_word(False), 'No')
