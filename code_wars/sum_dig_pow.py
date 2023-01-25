8b69abf1ffcccc632614da30c3eb808afbbdd1e6
tuyojr <olutuyod@gmail.com>
2023-01-25 09:09:03 +0100

    6kyu kata solution for Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!

--- code_wars/sum_dig_pow.py
+++ code_wars/sum_dig_pow.py
@@ -62,6 +62,8 @@ def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
             sum_list.append(num)
         
     return sum_list
+    
+    # one line solution
     # return [x for x in range(a, b+1) if sum(int(d)**i for i, d in enumerate(str(x), 1)) == x]
 
 # from solution import sum_dig_pow
