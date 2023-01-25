aa0a3174d022c7f39747b4148a9579035ae368f0
tuyojr <olutuyod@gmail.com>
2023-01-25 09:26:06 +0100

    sample tests

--- code_wars/get_volume_of_cuboid.py
+++ code_wars/get_volume_of_cuboid.py
@@ -1,3 +1,15 @@
 # Bob needs a fast way to calculate the volume of a cuboid 
 # with three values: the length, width and height of the cuboid. 
 # Write a function to help Bob with this calculation.
+
+
+
+# import codewars_test as test
+# from solution import get_volume_of_cuboid
+
+# @test.describe('Testing...')
+# def _():
+#     @test.it('Sample tests')
+#     def _():   
+#         test.assert_equals(get_volume_of_cuboid(1, 2, 2), 4)
+#         test.assert_equals(get_volume_of_cuboid(6.3, 2, 5), 63)
