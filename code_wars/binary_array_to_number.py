9448e3796de71122fa90639bc5817767f890dafb
tuyojr <olutuyod@gmail.com>
2023-01-26 09:12:58 +0100

    sample tests

--- code_wars/binary_array_to_number.py
+++ code_wars/binary_array_to_number.py
@@ -13,3 +13,17 @@
 # Testing: [1, 1, 1, 1] ==> 15
 # Testing: [1, 0, 1, 1] ==> 11
 # However, the arrays can have varying lengths, not just limited to 4.
+
+
+
+# import codewars_test as test
+# from solution import binary_array_to_number
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(binary_array_to_number([0,0,0,1]), 1)
+#         test.assert_equals(binary_array_to_number([0,0,1,0]), 2)
+#         test.assert_equals(binary_array_to_number([1,1,1,1]), 15)
+#         test.assert_equals(binary_array_to_number([0,1,1,0]), 6)
