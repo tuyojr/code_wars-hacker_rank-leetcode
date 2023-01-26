2483b2edba64f11b289a6501cceaff9b4496de4b
tuyojr <olutuyod@gmail.com>
2023-01-26 09:20:05 +0100

    sample tests

--- code_wars/string_to_array.py
+++ code_wars/string_to_array.py
@@ -4,3 +4,18 @@
 # "Robin Singh" ==> ["Robin", "Singh"]
 
 # "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+
+# import codewars_test as test
+# from solution import string_to_array
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(string_to_array("Robin Singh"), ["Robin", "Singh"])
+#         test.assert_equals(string_to_array("CodeWars"), ["CodeWars"])
+#         test.assert_equals(string_to_array("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"])
+#         test.assert_equals(string_to_array("1 2 3"), ["1", "2", "3"])
+#         test.assert_equals(string_to_array(""), [""])
