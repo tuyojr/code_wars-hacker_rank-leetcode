0d744727b1cce440d53976ed846d3bd676739cf3
tuyojr <olutuyod@gmail.com>
2023-01-28 08:31:27 +0100

    sample tests

--- code_wars/first_non_consecutive.py
+++ code_wars/first_non_consecutive.py
@@ -29,3 +29,20 @@
 # Python, Rust, Scala: None
 # Julia: nothing
 # Nim: none(int) (See options) https://nim-lang.org/docs/options.html
+
+
+
+# import codewars_test as test
+# from solution import first_non_consecutive
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(first_non_consecutive([1,2,3,4,6,7,8]), 6)
+#         test.assert_equals(first_non_consecutive([1,2,3,4,5,6,7,8]), None)
+#         test.assert_equals(first_non_consecutive([4,6,7,8,9,11]), 6)
+#         test.assert_equals(first_non_consecutive([4,5,6,7,8,9,11]), 11)
+#         test.assert_equals(first_non_consecutive([31,32]), None)
+#         test.assert_equals(first_non_consecutive([-3,-2,0,1]), 0)
+#         test.assert_equals(first_non_consecutive([-5,-4,-3,-1]), -1)
