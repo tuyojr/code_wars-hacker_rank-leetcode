e0ce4eee63e73984b5cbe97224866ce44cd813ac
tuyojr <olutuyod@gmail.com>
2023-01-28 08:37:43 +0100

    sample tests

--- code_wars/string_to_number.py
+++ code_wars/string_to_number.py
@@ -14,3 +14,17 @@
 # "605"  --> 605
 # "1405" --> 1405
 # "-7" --> -7
+
+
+
+# import codewars_test as test
+# from solution import string_to_number
+
+# @test.describe("string_to_number")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(string_to_number("1234"), 1234)
+#         test.assert_equals(string_to_number("605"), 605)
+#         test.assert_equals(string_to_number("1405"), 1405)
+#         test.assert_equals(string_to_number("-7"), -7)
