6adc3df518c210b2dece47bdedd7220b30857716
tuyojr <olutuyod@gmail.com>
2023-01-30 09:11:18 +0100

    count the divisors of a number 7kyu kata solution

--- code_wars/divisors.py
+++ code_wars/divisors.py
@@ -11,7 +11,23 @@
 # The numbers between parentheses are shown only for you to see 
 # which numbers are counted in each case.
 
+def divisors(n):
+    
+    # create a list to hold each possible divisor
+    divisor = []
+    
+    # loop through the range of numbers from 1 to the value of n + 1
+    for i in range(1, n + 1):
+        
+        # if the number can divide n without a remainder, add it to the list
+        if n % i == 0:
+            divisor.append(i)
+            
+    # return the list of divisors
+    return len(divisor)
 
+    # one line code
+    # return len([i for i in range(1, n + 1) if n % i == 0])
 
 # import codewars_test as test
 # from solution import divisors
