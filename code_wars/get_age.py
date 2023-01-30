cb5f2d6dc836eaf76d2d2291726572fc4956d7d4
tuyojr <olutuyod@gmail.com>
2023-01-30 08:57:48 +0100

    sample tests

--- code_wars/get_age.py
+++ code_wars/get_age.py
@@ -6,3 +6,17 @@
 # Assume the test input string is always a valid string. For example, 
 # the test input may be "1 year old" or "5 years old". The first 
 # character in the string is always a number.
+
+
+
+# import codewars_test as test
+# from solution import get_age
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(get_age("2 years old"), 2)
+#         test.assert_equals(get_age("4 years old"), 4)
+#         test.assert_equals(get_age("5 years old"), 5)
+#         test.assert_equals(get_age("7 years old"), 7)
