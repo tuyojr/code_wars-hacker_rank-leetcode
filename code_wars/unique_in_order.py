74f8630c71884d2561bbb738a01d7b033ea2de08
tuyojr <olutuyod@gmail.com>
2023-01-31 09:33:55 +0100

    unique in order 6kyu kata solution

--- code_wars/unique_in_order.py
+++ code_wars/unique_in_order.py
@@ -10,8 +10,25 @@
 # unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
 # unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
 
-
-
+def unique_in_order(sequence):
+    
+    # create a list to store the ordered elements
+    result = []
+    
+    # a variable to store the previous character
+    prev = None
+    
+    # loop through the input string
+    for char in sequence:
+        
+        # if the character is not equal to the previous character, add it to the final list
+        # and set it as the new value of the previous character
+        if char != prev:
+            result.append(char)
+            prev = char
+    
+    # return the new unique list
+    return result
 # import codewars_test as test
 # from solution import unique_in_order
 
