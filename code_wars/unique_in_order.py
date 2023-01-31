fb493e4fb90b74ca80fa142123a44c3c3cc2d729
tuyojr <olutuyod@gmail.com>
2023-01-31 09:22:31 +0100

    sample tests

--- code_wars/unique_in_order.py
+++ code_wars/unique_in_order.py
@@ -9,3 +9,37 @@
 # unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 # unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
 # unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
+
+
+
+# import codewars_test as test
+# from solution import unique_in_order
+
+
+# @test.describe("Sample Tests")
+# def sample_tests():
+#     @test.it("Should work with empty sequence")
+#     def _():
+#         test.assert_equals(unique_in_order(""), [])
+#         test.assert_equals(unique_in_order([]), [])
+#         test.assert_equals(unique_in_order(()), [])
+
+#     @test.it("Should work with single element sequence")
+#     def _():
+#         test.assert_equals(unique_in_order("A"), ["A"])
+#         test.assert_equals(unique_in_order(["A"]), ["A"])
+#         test.assert_equals(unique_in_order(("A",)), ["A"])
+
+#     @test.it("Should reduce duplicates")
+#     def _():
+#         test.assert_equals(unique_in_order("AA"), ["A"])
+#         test.assert_equals(unique_in_order("AAAABBBCCDAABBB"), ["A", "B", "C", "D", "A", "B"])
+
+#     @test.it("Should be case-sensitive")
+#     def _():
+#         test.assert_equals(unique_in_order("ABBCcA"), ["A", "B", "C", "c", "A"])
+
+#     @test.it("Should work with different element types")
+#     def _():
+#         test.assert_equals(unique_in_order([1, 2, 3, 3, -1]), [1, 2, 3, -1])
+#         test.assert_equals(unique_in_order(["a", "b", "b", "a"]), ["a", "b", "a"])
