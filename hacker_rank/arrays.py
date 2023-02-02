a91e30acb6ab9b3d0d6c8327bdbd4bdd966847ab
tuyojr <olutuyod@gmail.com>
2023-02-02 13:51:12 +0100

    numpy array challenge solution

--- hacker_rank/arrays.py
+++ hacker_rank/arrays.py
@@ -41,3 +41,14 @@
 # Sample Output
 
 # [-10.  -8.   4.   3.   2.   1.]
+
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    return numpy.array(arr[::-1], float)
+    
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
