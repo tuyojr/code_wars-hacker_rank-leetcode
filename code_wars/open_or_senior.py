c63e08bbd6700fecfad8f404cab9f38887d8b018
tuyojr <olutuyod@gmail.com>
2023-02-03 09:22:13 +0100

    categorize new member 7kyu kata solution

--- code_wars/open_or_senior.py
+++ code_wars/open_or_senior.py
@@ -20,7 +20,31 @@
 # input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 # output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
+def open_or_senior(data):
 
+    # create empty list to hold categories
+    category = []
+
+    # loop through data
+    for info in data:
+
+        # if age is greater than or equal to 55 and handicap is greater than 7
+        if info[0] >= 55 and info[1] > 7:
+
+            # append Senior to category
+            category.append("Senior")
+
+        # otherwise
+        else:
+
+            # append Open to category
+            category.append("Open")
+
+    # return category
+    return category
+
+    # one line solution
+    # return ["Senior" if age >= 55 and handicap > 7 else "Open" for (age, handicap) in data]
 
 # import codewars_test as test
 # from solution import open_or_senior
