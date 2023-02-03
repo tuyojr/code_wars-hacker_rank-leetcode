de62d2f8f40bfe951f20f36798d572fe3a27f374
tuyojr <olutuyod@gmail.com>
2023-02-03 09:14:52 +0100

    sample tests

--- code_wars/open_or_senior.py
+++ code_wars/open_or_senior.py
@@ -19,3 +19,15 @@
 # Example
 # input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 # output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+
+
+# import codewars_test as test
+# from solution import open_or_senior
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():    
+#         test.assert_equals(open_or_senior([(45, 12),(55,21),(19, -2),(104, 20)]),['Open', 'Senior', 'Open', 'Senior'])
+#         test.assert_equals(open_or_senior([(16, 23),(73,1),(56, 20),(1, -1)]),['Open', 'Open', 'Senior', 'Open'])
