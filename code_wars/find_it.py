d4edaaade13fab870b9160e23e0f3069d0a33014
tuyojr <olutuyod@gmail.com>
2023-02-03 09:35:00 +0100

    find the odd int 6ky kata solution

--- code_wars/find_it.py
+++ code_wars/find_it.py
@@ -9,7 +9,24 @@
 # [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 # [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
+def find_it(seq):
+    
+    # create a dictionary to keep track of all the integers and how many 
+    # times they appear
+    integers = {}
+    
+    # loop through each integer in the list and add it to the integers dictionary
+    for num in seq:
+        integers[num] = integers.get(num, 0) + 1
+        
+    # loop through the key and values in the dictionary and check if any key has
+    # an odd number value, then return that key
+    for key, value in integers.items():
+        if (value % 2) != 0:
+            return key
 
+    # one line solution
+    # return [x for x in seq if seq.count(x) % 2][0]
 
 # import codewars_test as test
 # from solution import find_it
