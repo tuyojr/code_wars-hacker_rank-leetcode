0183496b8417f6c71548d887f83cf6db76feb8bc
tuyojr <olutuyod@gmail.com>
2023-02-03 09:13:19 +0100

    ginortS challenge solution

--- hacker_rank/ginortS.py
+++ hacker_rank/ginortS.py
@@ -25,3 +25,51 @@
 # Sample Output
 
 # ginortS1324
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def sort_string(string):
+
+    # create a list for all the lowercase letters
+    lower = []
+
+    # create a list for all the uppercase letters
+    upper = []
+
+    # create a list for all the odd numbers
+    odd = []
+
+    # create a list for all the even numbers
+    even = []
+
+    # iterate through the string
+    for i in string:
+
+        # if the character is lowercase, add it to the lower list
+        if i.islower():
+            lower.append(i)
+
+        # if the character is uppercase, add it to the upper list
+        elif i.isupper():
+            upper.append(i)
+
+        # if the character is an odd number, add it to the odd list
+        elif int(i) % 2 == 0:
+            even.append(i)
+
+        # if the character is an even number, add it to the even list
+        else:
+            odd.append(i)
+
+    # sort the lists
+    lower.sort()
+    upper.sort()
+    odd.sort()
+    even.sort()
+
+    # return the sorted string in the required order
+    return ''.join(lower + upper + odd + even)
+
+if __name__ == '__main__':
+    s = input()
+    print(sort_string(s))
