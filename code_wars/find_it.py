0d130528bf911056a43c0542e489cb0507b36a0f
tuyojr <olutuyod@gmail.com>
2023-02-03 09:31:34 +0100

    sample tests

--- code_wars/find_it.py
+++ code_wars/find_it.py
@@ -8,3 +8,39 @@
 # [1,1,2] should return 2, because it occurs 1 time (which is odd).
 # [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 # [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+
+# import codewars_test as test
+# from solution import find_it
+
+# @test.describe("Sample tests")
+# def sample_tests():
+    
+#     @test.it("find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) should return 5 (because it appears 3 times)")
+#     def _():
+#         test.assert_equals(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
+        
+#     @test.it("find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) should return -1 (because it appears 1 time)")
+#     def _():
+#         test.assert_equals(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1);
+        
+#     @test.it("find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) should return 5 (because it appears 3 times)")
+#     def _():
+#         test.assert_equals(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5);
+        
+#     @test.it("find_it([10]) should return 10 (because it appears 1 time)")
+#     def _():
+#         test.assert_equals(find_it([10]), 10);
+
+#     @test.it("find_it([10, 10, 10]) should return 10 (because it appears 3 times)")
+#     def _():
+#         test.assert_equals(find_it([10, 10, 10]), 10);        
+        
+#     @test.it("find_it([1,1,1,1,1,1,10,1,1,1,1]) should return 10 (because it appears 1 time)")
+#     def _():
+#         test.assert_equals(find_it([1,1,1,1,1,1,10,1,1,1,1]), 10);
+
+#     @test.it("find_it([5,4,3,2,1,5,4,3,2,10,10]) should return 1 (because it appears 1 time)")
+#     def _():
+#         test.assert_equals(find_it([5,4,3,2,1,5,4,3,2,10,10]), 1);
