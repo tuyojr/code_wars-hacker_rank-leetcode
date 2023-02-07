2c4694a9cffe483ecd6eb15f8e7e942e21e7ac10
tuyojr <olutuyod@gmail.com>
2023-02-07 15:59:01 +0100

    Validating Email Addresses With a Filter challenge solution

--- hacker_rank/filter_emails.py
+++ hacker_rank/filter_emails.py
@@ -60,3 +60,23 @@
 # Sample Output
 
 # ['brian-23@hackerrank.com', 'britts_54@hackerrank.com', 'lara@hackerrank.com']
+
+import re
+
+def fun(s):
+    # return True if s is a valid email, else return False
+    pattern = re.compile(r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$')
+    return pattern.match(s)
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
