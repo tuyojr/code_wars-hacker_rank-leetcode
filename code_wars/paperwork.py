1f7200b7bd7889ce6b685d60fe0a0cd32c57e3cb
tuyojr <olutuyod@gmail.com>
2023-02-13 18:41:18 +0100

    sample tests

--- code_wars/paperwork.py
+++ code_wars/paperwork.py
@@ -8,3 +8,18 @@
 # n= 5, m=5: 25
 # n=-5, m=5:  0
 # Waiting for translations and Feedback! Thanks!
+
+
+
+# import codewars_test as test
+# from solution import paperwork
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(paperwork(5,5), 25, "Failed at Paperwork(5,5)")
+#         test.assert_equals(paperwork(-5,5), 0, "Failed at Paperwork(-5,5)")
+#         test.assert_equals(paperwork(5,-5), 0, "Failed at Paperwork(5,-5)")
+#         test.assert_equals(paperwork(-5,-5), 0, "Failed at Paperwork(-5,-5)")
+#         test.assert_equals(paperwork(5,0), 0, "Failed at Paperwork(5,0)")
