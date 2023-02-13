5221cf9d9148550d400fdb19372c0410a59b723f
tuyojr <olutuyod@gmail.com>
2023-02-13 18:07:39 +0100

    sample tests

--- code_wars/descending_order.py
+++ code_wars/descending_order.py
@@ -8,3 +8,20 @@
 # Input: 145263 Output: 654321
 
 # Input: 123456789 Output: 987654321
+
+
+
+# import codewars_test as test
+
+# try:
+#     from solution import Descending_Order as descending_order
+# except ImportError:
+#     from solution import descending_order
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(descending_order(0), 0)
+#         test.assert_equals(descending_order(15), 51)
+#         test.assert_equals(descending_order(123456789), 987654321)
