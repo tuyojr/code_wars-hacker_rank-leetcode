fc5f59ff2153f37d8cc855b2477b177aeab9be48
tuyojr <olutuyod@gmail.com>
2023-02-15 08:25:48 +0100

    sample tests

--- code_wars/remove_smallest.py
+++ code_wars/remove_smallest.py
@@ -22,3 +22,29 @@
 # * Input: [1,2,3,4,5], output = [2,3,4,5]
 # * Input: [5,3,2,1,4], output = [5,3,2,4]
 # * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+
+
+# test.describe("remove_smallest")
+
+# test.it("works for the examples")
+# test.assert_equals(remove_smallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]")
+# test.assert_equals(remove_smallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]")
+# test.assert_equals(remove_smallest([1, 2, 3, 1, 1]), [2, 3, 1, 1], "Wrong result for [1, 2, 3, 1, 1]")
+# test.assert_equals(remove_smallest([]), [], "Wrong result for []")
+
+# from numpy.random import randint
+
+# def randlist():
+#     return list(randint(400, size=randint(1, 10)))
+
+
+# test.it("returns [] if list has only one element")
+# for i in range(10):
+#     x = randint(1, 400)
+#     test.assert_equals(remove_smallest([x]), [], "Wrong result for [{}]".format(x))
+    
+# test.it("returns a list that misses only one element")
+# for i in range(10):
+#     arr = randlist()
+#     test.assert_equals(len(remove_smallest(arr[:])), len(arr) - 1, "Wrong sized result for {}".format(arr))
