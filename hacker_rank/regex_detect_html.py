079ad38fc1859d6f1b0861b6f393fd6615c72801
tuyojr <olutuyod@gmail.com>
2023-02-18 20:14:05 +0100

    Detect HTML Tags, Attributes and Attribute Values challenge solution

--- hacker_rank/regex_detect_html.py
+++ hacker_rank/regex_detect_html.py
@@ -76,3 +76,30 @@
 
 # param tag: Print the param tag. In the next 2 lines, print the attributes 
 # name along with their respective values.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from html.parser import HTMLParser
+
+class CustomHTMLParser(HTMLParser):
+    
+    def custom_print_attrs(self, attrs):
+        if attrs:
+            for attr in attrs:
+                print('->', attr[0], '>', attr[1])
+    
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        self.custom_print_attrs(attrs)
+
+    def handle_startendtag(self, tag, attrs):
+        print(tag)
+        self.custom_print_attrs(attrs)
+
+html_parser = CustomHTMLParser()
+pages = [input() for i in range(int(input()))]
+
+for page in pages:
+    html_parser.feed(page)
+
+# Solution by muhammadayubkhan
