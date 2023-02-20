0a33e560729f49413b5789f4be704bfd304c57fd
tuyojr <olutuyod@gmail.com>
2023-02-20 20:45:08 +0100

    XML2 - Find the Maximum Depth challenge solution

--- hacker_rank/depth.py
+++ hacker_rank/depth.py
@@ -31,3 +31,34 @@
 # The tags title, subtitle, link and updated all have a depth of 1.
 
 # Thus, the maximum depth is 1.
+
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+
+    # increment the level
+    level += 1
+
+    # check if the level is greater than the maxdepth
+    if level > maxdepth:
+
+        # if so, set the maxdepth to the level
+        maxdepth = level
+
+    # loop through the children of the element
+    for child in elem:
+
+        # call the depth function on the child
+        depth(child, level)
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
