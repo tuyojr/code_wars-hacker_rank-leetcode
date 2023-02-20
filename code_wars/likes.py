c3ff0c87953b38cc1c19465ae91b4d2ce11a96af
tuyojr <olutuyod@gmail.com>
2023-02-20 20:46:35 +0100

    sample tests

--- code_wars/likes.py
+++ code_wars/likes.py
@@ -12,3 +12,16 @@
 # ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 # ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 # Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+
+
+# import codewars_test as test
+# from solution import likes
+
+# @test.it('Basic tests')
+# def _():
+#     test.assert_equals(likes([]), 'no one likes this')
+#     test.assert_equals(likes(['Peter']), 'Peter likes this')
+#     test.assert_equals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+#     test.assert_equals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+#     test.assert_equals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
