0aeca7e3544c4884a1f8a9fd5de2b7350de7c1e7
tuyojr <olutuyod@gmail.com>
2023-02-20 20:50:34 +0100

    who likes it? 6kyu kata solution

--- code_wars/likes.py
+++ code_wars/likes.py
@@ -13,7 +13,18 @@
 # ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 # Note: For 4 or more names, the number in "and 2 others" simply increases.
 
-
+def likes(names):
+    # your code here
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return names[0] + " likes this"
+    elif len(names) == 2:
+        return names[0] + " and " + names[1] + " like this"
+    elif len(names) == 3:
+        return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+    else:
+        return names[0] + ", " + names[1] + " and " + str(len(names) - 2) + " others like this"
 
 # import codewars_test as test
 # from solution import likes
