19ca00390a24601708c43e04dcfa1318618aa850
tuyojr <olutuyod@gmail.com>
2023-02-21 16:29:56 +0100

    sample tests

--- code_wars/longest.py
+++ code_wars/longest.py
@@ -9,3 +9,15 @@
 
 # a = "abcdefghijklmnopqrstuvwxyz"
 # longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+
+
+# import codewars_test as test
+    
+# @test.describe("longest")
+# def tests():
+#     @test.it("basic tests")
+#     def basics():
+#         test.assert_equals(longest("aretheyhere", "yestheyarehere"), "aehrsty")
+#         test.assert_equals(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
+#         test.assert_equals(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
