f696494b44d4bb653979f317bc43afa38747eaa8
tuyojr <olutuyod@gmail.com>
2023-02-21 16:50:40 +0100

    sample tests

--- code_wars/update_light.py
+++ code_wars/update_light.py
@@ -7,3 +7,16 @@
 # string representing the state the light should change to.
 
 # For example, when the input is green, output should be yellow.
+
+
+
+# import codewars_test as test
+# from solution import update_light
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(update_light('green'), 'yellow')
+#         test.assert_equals(update_light('yellow'), 'red')
+#         test.assert_equals(update_light('red'), 'green')
