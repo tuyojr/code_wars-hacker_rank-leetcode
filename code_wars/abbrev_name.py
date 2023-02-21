8a0e1a70c881be5f541bd96ad9d528fdb10d1e14
tuyojr <olutuyod@gmail.com>
2023-02-21 16:39:55 +0100

    sample tests

--- code_wars/abbrev_name.py
+++ code_wars/abbrev_name.py
@@ -8,3 +8,22 @@
 # Sam Harris => S.H
 
 # patrick feeney => P.F
+
+
+
+# import codewars_test as test
+
+# try:
+#     from solution import abbrevName as abbrev_name
+# except ImportError:
+#     from solution import abbrev_name
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(abbrev_name("Sam Harris"), "S.H")
+#         test.assert_equals(abbrev_name("patrick feenan"), "P.F")
+#         test.assert_equals(abbrev_name("Evan C"), "E.C")
+#         test.assert_equals(abbrev_name("P Favuzzi"), "P.F")
+#         test.assert_equals(abbrev_name("David Mendieta"), "D.M")
