28805b40b8e578ce09d3bd9d5054dee2b1ed1749
tuyojr <olutuyod@gmail.com>
2023-02-22 08:44:53 +0100

    sum the strings 8kyu kata solution

--- code_wars/sum_str.py
+++ code_wars/sum_str.py
@@ -9,7 +9,13 @@
 # "2", "" --> "2"
 # "-5", "3" --> "-2"
 
-
+def sum_str(a, b):
+    # happy coding !
+    if a == "":
+        a = 0
+    if b == "":
+        b = 0
+    return str(int(a) + int(b))
 
 # import codewars_test as test
 # from solution import sum_str
