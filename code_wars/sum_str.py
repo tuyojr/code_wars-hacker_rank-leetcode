a3be33a1ff02d74f93feec80edc46ad6859abd84
tuyojr <olutuyod@gmail.com>
2023-02-22 08:35:28 +0100

    sample tests

--- code_wars/sum_str.py
+++ code_wars/sum_str.py
@@ -8,3 +8,22 @@
 # "", "" --> "0"
 # "2", "" --> "2"
 # "-5", "3" --> "-2"
+
+
+
+# import codewars_test as test
+# from solution import sum_str
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+    
+#     @test.it("Sample Tests")
+#     def sample_tests():
+#         test.assert_equals(sum_str("4","5"), "9")
+#         test.assert_equals(sum_str("34","5"), "39")
+
+#     @test.it("Tests with empty strings")
+#     def empty_string():
+#         test.assert_equals(sum_str("9",""), "9", "x + empty = x")
+#         test.assert_equals(sum_str("","9"), "9", "empty + x = x")
+#         test.assert_equals(sum_str("","") , "0", "empty + empty = 0")
