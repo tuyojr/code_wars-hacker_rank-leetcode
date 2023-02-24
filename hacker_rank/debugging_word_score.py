d8eca57e169cb967c34b540f6d52dec42b21da59
tuyojr <olutuyod@gmail.com>
2023-02-24 08:30:08 +0100

    debugging word score challenge solution

--- hacker_rank/debugging_word_score.py
+++ hacker_rank/debugging_word_score.py
@@ -61,3 +61,26 @@
 # is 1 since it contains 3 vowels, an odd number of vowels. The score of is is also 1 
 # because it has an odd number of vowels. The score of awesome is 2 since it contains 
 # 4 vowels, an even number of vowels. Thus, the total score is 1 + 1 + 2 = 4.
+
+def is_vowel(letter):
+    return letter in ['a', 'e', 'i', 'o', 'u', 'y']
+
+def score_words(words):
+    score = 0
+    for word in words:
+        num_vowels = 0
+        for letter in word:
+            if is_vowel(letter):
+                num_vowels += 1
+        if num_vowels % 2 == 0:
+            score += 2
+        else:
+
+            # line corrected from ++score
+            score += 1
+    return score
+
+
+n = int(input())
+words = input().split()
+print(score_words(words))
