b714e6215af1d7c2b0ebe65b58dd1057187a0ed1
tuyojr <olutuyod@gmail.com>
2023-03-02 08:19:02 +0100

    numpy sum and prod challenge solution

--- hacker_rank/numpy_sum_prod.py
+++ hacker_rank/numpy_sum_prod.py
@@ -59,3 +59,11 @@
 
 # The sum along axis 0 = [4 6]
 # The product of this sum = 24
+
+import numpy
+
+N, M = map(int, input().split())
+
+A = numpy.array([input().split() for _ in range(N)], int)
+
+print(numpy.prod(numpy.sum(A, axis=0), axis=0))
