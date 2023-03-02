d4987eb0b0024ecbc91df9358ddfa1dd354b7970
tuyojr <olutuyod@gmail.com>
2023-03-02 08:43:03 +0100

    numpy dot and cross challenge solution

--- hacker_rank/numpy_dot_cross.py
+++ hacker_rank/numpy_dot_cross.py
@@ -48,3 +48,12 @@
 
 # [[ 7 10]
 #  [15 22]]
+
+import numpy
+
+N = int(input())
+
+A = numpy.array([input().split() for _ in range(N)], int)
+B = numpy.array([input().split() for _ in range(N)], int)
+
+print(numpy.dot(A, B))
