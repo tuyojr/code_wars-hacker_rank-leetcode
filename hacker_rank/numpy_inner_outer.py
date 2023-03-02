0184cbb05c61712da66cc1f49d0c4dafaf63e2f2
tuyojr <olutuyod@gmail.com>
2023-03-02 08:45:33 +0100

    numpy inner and outer products challenge description

---
+++ hacker_rank/numpy_inner_outer.py
@@ -0,0 +1,50 @@
+# inner
+# https://numpy.org/doc/stable/reference/generated/numpy.inner.html
+# The inner tool returns the inner product of two arrays.
+# https://en.wikipedia.org/wiki/Inner_product_space
+
+# import numpy
+
+# A = numpy.array([0, 1])
+# B = numpy.array([3, 4])
+
+# print numpy.inner(A, B)     #Output : 4
+
+# outer
+# https://numpy.org/doc/stable/reference/generated/numpy.outer.html
+# The outer tool returns the outer product of two arrays.
+# https://en.wikipedia.org/wiki/Outer_product
+
+# import numpy
+
+# A = numpy.array([0, 1])
+# B = numpy.array([3, 4])
+
+# print numpy.outer(A, B)     #Output : [[0 0]
+#                             #          [3 4]]
+
+# Task
+
+# You are given two arrays: A and B.
+# Your task is to compute their inner and outer product.
+
+# Input Format
+
+# The first line contains the space separated elements of array A.
+# The second line contains the space separated elements of array B.
+
+# Output Format
+
+# First, print the inner product.
+# Second, print the outer product.
+
+# Sample Input
+
+# 0 1
+# 2 3
+
+# Sample Output
+
+# 3
+# [[0 0]
+#  [2 3]]
