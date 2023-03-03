c921952258a2f530834cab7a084b2ba65ab0e20c
tuyojr <olutuyod@gmail.com>
2023-03-03 04:25:24 +0100

    Extract the domain name from a URL 5kyu kata solution

--- code_wars/domain_name.py
+++ code_wars/domain_name.py
@@ -5,7 +5,18 @@
 # * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 # * url = "https://www.cnet.com"                -> domain name = cnet"
 
-
+def domain_name(url):
+    if 'www' in url:
+        url = url.split('www.')
+        url = url[1].split('.')
+        return url[0]
+    elif '//' in url:
+        url = url.split('//')
+        url = url[1].split('.')
+        return url[0]
+    else:
+        url = url.split('.')
+        return url[0]
 
 # test.assert_equals(domain_name("http://google.com"), "google")
 # test.assert_equals(domain_name("http://google.co.jp"), "google")
