8df71df05868a18ebe165146dfe3e30832582447
tuyojr <olutuyod@gmail.com>
2023-03-03 04:24:25 +0100

    sample tests

--- code_wars/domain_name.py
+++ code_wars/domain_name.py
@@ -4,3 +4,10 @@
 # * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 # * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 # * url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+
+# test.assert_equals(domain_name("http://google.com"), "google")
+# test.assert_equals(domain_name("http://google.co.jp"), "google")
+# test.assert_equals(domain_name("www.xakep.ru"), "xakep")
+# test.assert_equals(domain_name("https://youtube.com"), "youtube")
