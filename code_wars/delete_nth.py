27a1bccf9e582beb7ef6211aa7da411acdcfbd35
tuyojr <olutuyod@gmail.com>
2023-03-05 18:36:17 +0100

    sample tests

--- code_wars/delete_nth.py
+++ code_wars/delete_nth.py
@@ -18,3 +18,8 @@
 # since this would lead to 1 and 2 being in the result 3 times, 
 # and then take 3, which leads to [1,2,3,1,2,3].
 # With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+
+
+# test.assert_equals(delete_nth([20,37,20,21], 1), [20,37,21])
+# test.assert_equals(delete_nth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
