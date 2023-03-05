2b4997c2cb1400d5365d081cdb75cbc4a4788827
tuyojr <olutuyod@gmail.com>
2023-03-05 18:47:11 +0100

    sample tests

--- code_wars/positive_sum.py
+++ code_wars/positive_sum.py
@@ -3,3 +3,24 @@
 # Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 # Note: if there is nothing to sum, the sum is default to 0.
+
+
+
+# import codewars_test as test
+# from solution import positive_sum
+
+# @test.describe("positive_sum")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(positive_sum([1,2,3,4,5]),15)
+#         test.assert_equals(positive_sum([1,-2,3,4,5]),13)
+#         test.assert_equals(positive_sum([-1,2,3,4,-5]),9)
+        
+#     @test.it("returns 0 when array is empty")
+#     def empty_case():
+#         test.assert_equals(positive_sum([]),0)      
+        
+#     @test.it("returns 0 when all elements are negative")
+#     def negative_case():
+#         test.assert_equals(positive_sum([-1,-2,-3,-4,-5]),0)
