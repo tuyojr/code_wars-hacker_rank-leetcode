82152f1a87758f7da7e96889557448de2d234dd3
tuyojr <olutuyod@gmail.com>
2023-03-06 20:45:46 +0100

    sample tests

--- code_wars/two_sort.py
+++ code_wars/two_sort.py
@@ -6,3 +6,18 @@
 # its letters.
 
 # You should not remove or add elements from/to the array.
+
+
+
+# import codewars_test as test
+# from solution import two_sort
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(two_sort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' )
+#         test.assert_equals(two_sort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e')
+#         test.assert_equals(two_sort(["lets", "talk", "about", "javascript", "the", "best", "language"]), 'a***b***o***u***t')
+#         test.assert_equals(two_sort(["i", "want", "to", "travel", "the", "world", "writing", "code", "one", "day"]), 'c***o***d***e')
+#         test.assert_equals(two_sort(["Lets", "all", "go", "on", "holiday", "somewhere", "very", "cold"]), 'L***e***t***s')
