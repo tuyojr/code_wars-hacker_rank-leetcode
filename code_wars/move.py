0f2cda002bba83fc76f78dbbe3182af64de4f6a0
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-03-07 14:48:42 +0100

    sample tests

--- code_wars/move.py
+++ code_wars/move.py
@@ -7,3 +7,16 @@
 
 # Example:
 # move(3, 6) should equal 15
+
+
+
+# import codewars_test as test
+# from solution import move
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(move(0, 4), 8)
+#         test.assert_equals(move(3, 6), 15)
+#         test.assert_equals(move(2, 5), 12)
