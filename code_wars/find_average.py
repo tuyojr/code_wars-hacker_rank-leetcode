f3109ea36df1908e7874924afdb8d5a051e7a449
tuyojr <olutuyod@gmail.com>
2023-03-09 13:10:01 +0100

    calculate average 8kyu kata solution

--- code_wars/find_average.py
+++ code_wars/find_average.py
@@ -2,3 +2,16 @@
 # in a given list.
 
 # Note: Empty arrays should return 0.
+
+def find_average(numbers):
+    # your code here
+    return 0 if len(numbers) == 0 else (sum(numbers) / len(numbers))
+
+# import codewars_test as test
+# from solution import find_average
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(find_average([1, 2, 3]), 2)
