d767c7f14a790c8c61ae43a75c7ce1763cc2c8e2
tuyojr <olutuyod@gmail.com>
2023-03-09 13:07:44 +0100

    sort the odd 6kyu kata solution

--- code_wars/sort_array.py
+++ code_wars/sort_array.py
@@ -8,7 +8,19 @@
 # [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
+def sort_array(source_array):
+    # Return a sorted array.
+    odd = []
 
+    for i in source_array:
+        if i % 2 != 0:
+            odd.append(i)
+    odd.sort()
+
+    for i in range(len(source_array)):
+        if source_array[i] % 2 != 0:
+            source_array[i] = odd.pop(0)
+    return source_array
 
 # test.assert_equals(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
 # test.assert_equals(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
