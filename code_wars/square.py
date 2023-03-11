da75d15859794f647dbb6915388e2fcf119bcead
tuyojr <olutuyod@gmail.com>
2023-03-11 20:09:58 +0100

    sample tests

--- code_wars/square.py
+++ code_wars/square.py
@@ -1,1 +1,14 @@
 # Now you have to write a function that takes an argument and returns the square of it.
+
+
+
+# import codewars_test as test
+# from solution import square
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(square(2), 4)
+#         test.assert_equals(square(50), 2500)
+#         test.assert_equals(square(1), 1)
