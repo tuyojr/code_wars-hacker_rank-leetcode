82e06327cb77e1d58e62459f574257bd1e0dec17
tuyojr <olutuyod@gmail.com>
2023-03-11 20:13:40 +0100

    sample tests

--- code_wars/is_even.py
+++ code_wars/is_even.py
@@ -7,3 +7,24 @@
 # Numbers may be positive or negative, integers or floats.
 
 # Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+
+
+# import codewars_test as test
+# from solution import is_even
+
+# @test.describe('Fixed Tests')
+# def fixed_tests():
+
+#     @test.it('Basic Test Cases')
+#     def basic_tests():
+#         test.assert_equals(is_even(0), True)
+#         test.assert_equals(is_even(0.5), False)
+#         test.assert_equals(is_even(1), False)
+#         test.assert_equals(is_even(2), True)
+#         test.assert_equals(is_even(-4), True)
+#         test.assert_equals(is_even(15), False)
+#         test.assert_equals(is_even(20), True)
+#         test.assert_equals(is_even(220), True)
+#         test.assert_equals(is_even(222222221), False)
+#         test.assert_equals(is_even(500000000), True)
