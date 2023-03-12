e46c520a44bd0eec5758ee991fcbfaac79579ac0
tuyojr <olutuyod@gmail.com>
2023-03-12 08:51:23 +0100

    sample tests

--- code_wars/make_upper_case.py
+++ code_wars/make_upper_case.py
@@ -1,1 +1,12 @@
 # Write a function which converts the input string to uppercase.
+
+
+
+# import codewars_test as test
+# from solution import make_upper_case
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(make_upper_case("hello"), "HELLO")
