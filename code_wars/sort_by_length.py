581037250c6005cbee40aaef3adc52a978dff8c9
tuyojr <olutuyod@gmail.com>
2023-03-13 15:29:55 +0100

    sample tests

--- code_wars/sort_by_length.py
+++ code_wars/sort_by_length.py
@@ -13,3 +13,25 @@
 # All of the strings in the array passed to your function will be different 
 # lengths, so you will not have to decide how to order multiple strings of 
 # the same length.
+
+
+
+# import codewars_test as test
+# from solution import sort_by_length
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+
+#         tests = [
+#             [["i", "to", "beg", "life"], ["beg", "life", "i", "to"]],
+#             [["", "pizza", "brains", "moderately"], ["", "moderately", "brains", "pizza"]],
+#             [["short", "longer", "longest"], ["longer", "longest", "short"]],
+#             [["a", "of", "dog", "food"], ["dog", "food", "a", "of"]],
+#             [["", "bees", "eloquent", "dictionary"], ["", "dictionary", "eloquent", "bees"]],
+#             [["a short sentence", "a longer sentence", "the longest sentence"], ["a longer sentence", "the longest sentence", "a short sentence"]],
+#         ]
+        
+#         for exp, inp in tests:
+#             test.assert_equals(sort_by_length(inp), exp)
