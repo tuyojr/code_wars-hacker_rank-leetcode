e2b03295d0b6e41c0d0ce664ea66e6444a563947
tuyojr <olutuyod@gmail.com>
2023-04-10 10:47:52 +0100

    product of array except self medium challenge solution

--- leetcode/productExceptSelf.py
+++ leetcode/productExceptSelf.py
@@ -30,3 +30,25 @@
 
 # Follow up: Can you solve the problem in O(1) extra space complexity? 
 # (The output array does not count as extra space for space complexity analysis.)
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        n = len(nums)
+        answer = [1] * n
+    
+        # Calculate prefix products
+        for i in range(1, n):
+            answer[i] = answer[i-1] * nums[i-1]
+    
+        # Calculate suffix products and answer
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+    
+        return answer
