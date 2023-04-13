cd205f3ecd43bb784954938a44e916d80db3e160
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-13 08:38:53 +0100

    sample tests

--- code_wars/alternating_split.py
+++ code_wars/alternating_split.py
@@ -16,3 +16,27 @@
 
 # If the string S is an empty value or the integer N is not positive, return the first argument 
 # without changes.
+
+
+
+# test.describe('Basic Tests')
+# test.assert_equals(encrypt("This is a test!", 0), "This is a test!")
+# test.assert_equals(encrypt("This is a test!", 1), "hsi  etTi sats!")
+# test.assert_equals(encrypt("This is a test!", 2), "s eT ashi tist!")
+# test.assert_equals(encrypt("This is a test!", 3), " Tah itse sits!")
+# test.assert_equals(encrypt("This is a test!", 4), "This is a test!")
+# test.assert_equals(encrypt("This is a test!", -1), "This is a test!")
+# test.assert_equals(encrypt("This kata is very interesting!", 1), "hskt svr neetn!Ti aai eyitrsig")
+
+# test.assert_equals(decrypt("This is a test!", 0), "This is a test!")
+# test.assert_equals(decrypt("hsi  etTi sats!", 1), "This is a test!")
+# test.assert_equals(decrypt("s eT ashi tist!", 2), "This is a test!")
+# test.assert_equals(decrypt(" Tah itse sits!", 3), "This is a test!")
+# test.assert_equals(decrypt("This is a test!", 4), "This is a test!")
+# test.assert_equals(decrypt("This is a test!", -1), "This is a test!")
+# test.assert_equals(decrypt("hskt svr neetn!Ti aai eyitrsig", 1), "This kata is very interesting!")
+
+# test.assert_equals(encrypt("", 0), "")
+# test.assert_equals(decrypt("", 0), "")
+# test.assert_equals(encrypt(None, 0), None)
+# test.assert_equals(decrypt(None, 0), None)
