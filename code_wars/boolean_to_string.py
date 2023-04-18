bf39f9e1e28f47afc4403f5055a4b0a749b7c0a3
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-18 09:13:12 +0100

    sample tests

--- code_wars/boolean_to_string.py
+++ code_wars/boolean_to_string.py
@@ -1,3 +1,15 @@
 # Implement a function which convert the given boolean value into its string representation.
 
 # Note: Only valid inputs will be given.
+
+
+
+# import codewars_test as test
+# from solution import boolean_to_string
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(boolean_to_string(True), "True")
+#         test.assert_equals(boolean_to_string(False), "False")
