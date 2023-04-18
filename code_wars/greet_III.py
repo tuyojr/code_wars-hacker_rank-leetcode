1c9aa98215b8e04aeef92cc3a87952509b36e24b
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-18 09:20:32 +0100

    sample tests

--- code_wars/greet_III.py
+++ code_wars/greet_III.py
@@ -2,3 +2,15 @@
 # your program should return, "Hello, <name> how are you doing today?".
 
 # [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+
+# import codewars_test as test
+# from solution import greet
+
+# @test.describe("Fixed Tests")
+# def basic_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(greet('Ryan'), "Hello, Ryan how are you doing today?")
+#         test.assert_equals(greet('Shingles'), "Hello, Shingles how are you doing today?")
