47253468480333bf8b71c33a12b975719f020a2f
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-18 10:39:19 +0100

    sample tests

--- code_wars/set_alarm.py
+++ code_wars/set_alarm.py
@@ -10,3 +10,17 @@
 # setAlarm(false, true) -> false
 # setAlarm(false, false) -> false
 # setAlarm(true, false) -> true
+
+
+
+# import codewars_test as test
+# from solution import set_alarm
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(set_alarm(True, True), False, "Fails when input is True, True")
+#         test.assert_equals(set_alarm(False, True), False, "Fails when input is False, True")
+#         test.assert_equals(set_alarm(False, False), False, "Fails when input is False, False")
+#         test.assert_equals(set_alarm(True, False), True, "Fails when input is True, False")
