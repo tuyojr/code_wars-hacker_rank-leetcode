e44ba02e0a145cf0acffff17333385baad724b45
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-18 09:26:23 +0100

    Grasshopper - Basic Function Fixer 8kyu kata description

---
+++ code_wars/add_five.py
@@ -0,0 +1,9 @@
+# Fix the function
+# I created this function to add five to any number that was passed in to it and 
+# return the new value. It doesn't throw any errors but it returns the wrong number.
+
+# def add_five(num):
+#     total = num + 5
+#     return num
+
+# Can you help me fix the function?
