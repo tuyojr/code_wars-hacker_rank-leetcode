a95a266eca6cb1fb4d49a608d97d2761fb6d1892
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-20 11:27:39 +0100

    sample tests

--- code_wars/count.py
+++ code_wars/count.py
@@ -2,3 +2,18 @@
 # have a string like aba, then the result should be {'a': 2, 'b': 1}.
 
 # What if the string is empty? Then the result should be empty object literal, {}.
+
+
+
+# import codewars_test as test
+# from solution import count
+
+# @test.describe("Basic Tests")
+# def basic_tests():
+    
+#     @test.it("Basic Tests")
+#     def basic_tests():
+#         test.assert_equals(count('aba'), {'a': 2, 'b': 1})
+#         test.assert_equals(count(''), {})
+#         test.assert_equals(count('aa'), {'a' : 2})
+#         test.assert_equals(count('aabb'), {'b' : 2, 'a' : 2})
