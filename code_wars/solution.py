fe15029adbe89bca665f83c87c26d701418dbbfa
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-20 10:12:02 +0100

    sample tests

--- code_wars/solution.py
+++ code_wars/solution.py
@@ -5,3 +5,9 @@
 # "camelCasing"  =>  "camel Casing"
 # "identifier"   =>  "identifier"
 # ""             =>  ""
+
+
+
+# test.assert_equals(solution("helloWorld"), "hello World")
+# test.assert_equals(solution("camelCase"), "camel Case")
+# test.assert_equals(solution("breakCamelCase"), "break Camel Case")
