382d4ee6389dc4dd9c129a45982c46407da6debd
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-25 12:46:07 +0000

    sample tests

--- code_wars/is_uppercase.py
+++ code_wars/is_uppercase.py
@@ -17,15 +17,13 @@ considered to be in ALL CAPS.
 
 
 
-"""
-def gen_test_case(inp, res):
-    test.assert_equals(is_uppercase(inp), res, inp)
+# def gen_test_case(inp, res):
+#     test.assert_equals(is_uppercase(inp), res, inp)
 
-test.describe("Basic Tests")
+# test.describe("Basic Tests")
 
-gen_test_case("c", False)
-gen_test_case("C", True)
-gen_test_case("hello I AM DONALD", False)
-gen_test_case("HELLO I AM DONALD", True)
-gen_test_case("$%&", True)
-"""
+# gen_test_case("c", False)
+# gen_test_case("C", True)
+# gen_test_case("hello I AM DONALD", False)
+# gen_test_case("HELLO I AM DONALD", True)
+# gen_test_case("$%&", True)
