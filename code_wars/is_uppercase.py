37e360eb9de05e026f56912f7586d1fb81977c39
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-25 12:45:26 +0000

    sample tests

--- code_wars/is_uppercase.py
+++ code_wars/is_uppercase.py
@@ -14,3 +14,18 @@ In this Kata, a string is said to be in ALL CAPS whenever it does not contain
 any lowercase letter so any string containing no letters at all is trivially 
 considered to be in ALL CAPS.
 """
+
+
+
+"""
+def gen_test_case(inp, res):
+    test.assert_equals(is_uppercase(inp), res, inp)
+
+test.describe("Basic Tests")
+
+gen_test_case("c", False)
+gen_test_case("C", True)
+gen_test_case("hello I AM DONALD", False)
+gen_test_case("HELLO I AM DONALD", True)
+gen_test_case("$%&", True)
+"""
