33805e6d776d55da248eae3011b14fc162b749e8
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-25 11:34:30 +0100

    vowel remover 8kyu kata description

--- code_wars/shortcut.py
+++ code_wars/shortcut.py
@@ -9,6 +9,9 @@
 # don't worry about uppercase vowels
 # y is not considered a vowel for this kata
 
-
+def shortcut( s ):
+    # ...
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    return ''.join([l for l in s if l not in vowels]);
 
 # test.assert_equals(shortcut('hello'),'hll')
