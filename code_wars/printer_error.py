e79e855527a5ab0fb839fae8020a39780a6d3ecd
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-04-25 08:53:33 +0100

    sample tests

--- code_wars/printer_error.py
+++ code_wars/printer_error.py
@@ -21,3 +21,19 @@
 
 # s="aaaxbbbbyyhwawiwjjjwwm"
 # printer_error(s) => "8/22"
+
+
+
+# import codewars_test as test
+# from solution import printer_error
+
+# @test.describe("printer_error")
+# def basic_tests():
+#     @test.it('Example Test Cases')
+#     def example_test_cases():
+#         s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+#         test.assert_equals(printer_error(s), "3/56")
+#         s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+#         test.assert_equals(printer_error(s), "6/60")
+#         s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu"
+#         test.assert_equals(printer_error(s) , "11/65")
