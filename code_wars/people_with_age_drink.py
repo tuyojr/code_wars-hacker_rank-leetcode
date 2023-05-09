2a2def1372405db3f73d6d691462a316f589df66
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-05-09 07:44:08 +0000

    sample tests

--- code_wars/people_with_age_drink.py
+++ code_wars/people_with_age_drink.py
@@ -17,3 +17,31 @@
 # 18 --> "drink beer"
 # 20 --> "drink beer"
 # 30 --> "drink whisky"
+
+
+
+# import codewars_test as test
+# from solution import people_with_age_drink
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it("should return 'drink toddy' when age is less than 14")
+#     def _():
+#         test.assert_equals(people_with_age_drink(13), 'drink toddy', "Wrong result for 13")
+#         test.assert_equals(people_with_age_drink(0), 'drink toddy', "Wrong result for 0")
+
+#     @test.it("should return 'drink coke' when age is between 14(inclusive) and 18(exclusive)")
+#     def _():
+#         test.assert_equals(people_with_age_drink(17), 'drink coke')
+#         test.assert_equals(people_with_age_drink(15), 'drink coke')
+#         test.assert_equals(people_with_age_drink(14), 'drink coke')
+
+#     @test.it("should return 'drink beer' when age is between 18(inclusive) and 21(exclusive)")
+#     def _():
+#         test.assert_equals(people_with_age_drink(20), 'drink beer')
+#         test.assert_equals(people_with_age_drink(18), 'drink beer')
+
+#     @test.it("should return 'drink whisky' when age is greater than or equal to 21")
+#     def _():
+#         test.assert_equals(people_with_age_drink(22), 'drink whisky')
+#         test.assert_equals(people_with_age_drink(21), 'drink whisky')
