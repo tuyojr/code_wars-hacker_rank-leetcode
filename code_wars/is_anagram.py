82d2a97bbb5bcffb02671528bf58f75a8a5cb6d4
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-05-09 14:25:41 +0000

    sample tests

--- code_wars/is_anagram.py
+++ code_wars/is_anagram.py
@@ -10,3 +10,18 @@
 # "foefet" is an anagram of "toffee"
 
 # "Buckethead" is an anagram of "DeathCubeK"
+
+
+# import codewars_test as test
+# from solution import is_anagram
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(is_anagram("foefet", "toffee"), True, 'The word foefet is an anagram of toffee')
+#         test.assert_equals(is_anagram("Buckethead", "DeathCubeK"), True, 'The word Buckethead is an anagram of DeathCubeK')
+#         test.assert_equals(is_anagram("Twoo", "WooT"), True, 'The word Twoo is an anagram of WooT')
+#         test.assert_equals(is_anagram("dumble", "bumble"), False, 'Characters do not match for test case dumble, bumble')
+#         test.assert_equals(is_anagram("ound", "round"), False, 'Missing characters for test case ound, round')
+#         test.assert_equals(is_anagram("apple", "pale"), False, 'Same letters, but different count')
