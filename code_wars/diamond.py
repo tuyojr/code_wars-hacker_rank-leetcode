fc447525feff3833277e25048a3703cee79f9833
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-05-09 14:32:56 +0000

    give me a diamond 6kyu kata solution

--- code_wars/diamond.py
+++ code_wars/diamond.py
@@ -30,6 +30,21 @@
 
 # "  *\n ***\n*****\n ***\n  *\n"
 
+def diamond(n):
+    # Make some diamonds!
+    if n < 0 or n % 2 == 0:
+        return None
+    
+    result = "*" * n + "\n";
+    spaces = 1;
+    n = n - 2
+    while n > 0:
+        current = " " * spaces + "*" * n + "\n"
+        spaces = spaces + 1
+        n = n - 2
+        result = current + result + current
+    
+    return result
 
 # expected =  " *\n"
 # expected += "***\n"
