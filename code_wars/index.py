665e8b19e1a7f8ec9dcd299a311af1ffddabe968
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-05-10 14:15:12 +0000

    N-th power 8kyu kata solution

--- code_wars/index.py
+++ code_wars/index.py
@@ -10,7 +10,8 @@
 # array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 # array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 
-
+def index(array, n):
+    return array[n] ** n if n < len(array) else -1
 
 # import codewars_test as test
 # from solution import index
