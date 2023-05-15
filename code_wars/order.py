8f44548f0b6cdbeab7b5740821323dd59c08a1dc
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-05-15 06:57:35 +0000

    sample tests

--- code_wars/order.py
+++ code_wars/order.py
@@ -10,3 +10,15 @@
 # "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 # "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
 # ""  -->  ""
+
+
+# from solution import order
+# import codewars_test as test
+
+# @test.describe('Your order, please')
+# def desc1():
+#     @test.it('Sample tests')
+#     def it1():
+#         test.assert_equals(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+#         test.assert_equals(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+#         test.assert_equals(order(""), "")
