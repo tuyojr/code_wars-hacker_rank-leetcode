fce72f641dd44ee4269b157330e29624e356a540
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-05-16 10:53:29 +0000

    sample tests

--- code_wars/rental_car_cost.py
+++ code_wars/rental_car_cost.py
@@ -1,12 +1,25 @@
-# After a hard quarter in the office you decide to get some rest on a vacation. 
-# So you will book a flight for you and your girlfriend and try to leave all 
+# After a hard quarter in the office you decide to get some rest on a vacation.
+# So you will book a flight for you and your girlfriend and try to leave all
 # the mess behind you.
 
-# You will need a rental car in order for you to get around in your vacation. 
+# You will need a rental car in order for you to get around in your vacation.
 # The manager of the car rental makes you some good offers.
 
-# Every day you rent the car costs $40. If you rent the car for 7 or more days, 
-# you get $50 off your total. Alternatively, if you rent the car for 3 or more 
+# Every day you rent the car costs $40. If you rent the car for 7 or more days,
+# you get $50 off your total. Alternatively, if you rent the car for 3 or more
 # days, you get $20 off your total.
 
 # Write a code that gives out the total amount for different days(d).
+
+
+# import codewars_test as test
+# from solution import rental_car_cost
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(rental_car_cost(1),40)
+#         test.assert_equals(rental_car_cost(4),140)
+#         test.assert_equals(rental_car_cost(7),230)
+#         test.assert_equals(rental_car_cost(8),270)
