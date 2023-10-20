b48918d2d51c19e87aa47020b7ee864829098fc5
tuyojr <olutuyod@gmail.com>
2023-10-20 17:36:40 +0100

    sample tests

--- code_wars/remove_url_anchor.py
+++ code_wars/remove_url_anchor.py
@@ -3,3 +3,17 @@
 # Examples
 # "www.codewars.com#about" --> "www.codewars.com"
 # "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+
+
+
+# import codewars_test as test
+# from solution import remove_url_anchor
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(remove_url_anchor("www.codewars.com#about"), "www.codewars.com")
+#         test.assert_equals(remove_url_anchor("www.codewars.com/katas/?page=1#about"), "www.codewars.com/katas/?page=1")
+#         test.assert_equals(remove_url_anchor("www.codewars.com/katas/"), "www.codewars.com/katas/")
