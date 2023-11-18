9961c66103148b9ddd1d4d3f7e4693779ea028df
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2023-11-18 06:29:54 +0000

    sample tests

--- code_wars/factorial.py
+++ code_wars/factorial.py
@@ -21,4 +21,4 @@
 #         test.assert_equals(factorial(0), 1, "factorial for 0 is 1"),
 #         test.assert_equals(factorial(1), 1, "factorial for 1 is 1"),
 #         test.assert_equals(factorial(2), 2, "factorial for 2 is 2"),
-#         test.assert_equals(factorial(3), 6, "factorial for 3 is 6"),
+#         test.assert_equals(factorial(3), 6, "factorial for 3 is 6"), 
