f63fc7d0762e1973cebd8ae193cf99bdd7c07b83
tuyojr <olutuyod@gmail.com>
2023-11-26 07:28:38 +0100

    sample tests

--- code_wars/check_coupon.py
+++ code_wars/check_coupon.py
@@ -10,3 +10,13 @@
 
 # A coupon is no more valid on the day AFTER the expiration date. 
 # All dates will be passed as strings in this format: "MONTH DATE, YEAR"
+
+
+
+# from solution import check_coupon
+# import codewars_test as test
+
+# @test.it("Example tests")
+# def example_tests():
+#     test.assert_equals(check_coupon('123','123','September 5, 2014','October 1, 2014'), True);
+#     test.assert_equals(check_coupon('123a','123','September 5, 2014','October 1, 2014'), False);
