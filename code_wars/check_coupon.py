9ba1ff346c8b6f3bd19c252fce1941560acea297
tuyojr <olutuyod@gmail.com>
2023-11-27 08:09:27 +0100

    the coupon problem solution

--- code_wars/check_coupon.py
+++ code_wars/check_coupon.py
@@ -11,7 +11,12 @@
 # A coupon is no more valid on the day AFTER the expiration date. 
 # All dates will be passed as strings in this format: "MONTH DATE, YEAR"
 
+from datetime import datetime
 
+def check_coupon(entered_code, correct_code, current_date, expiration_date):
+    current_date_formatted = datetime.strptime(current_date, "%B %d, %Y")
+    expiration_date_formatted = datetime.strptime(current_date, "%B %d, %Y")
+    return entered_code is correct_code and expiration_date_formatted >= current_date_formatted
 
 # from solution import check_coupon
 # import codewars_test as test
