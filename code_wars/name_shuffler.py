dcbd9de8823d1ec6ddf08f8beee0f55112ae31a7
tuyojr <olutuyod@gmail.com>
2023-11-29 08:36:51 +0100

    sample tests

---
+++ code_wars/name_shuffler.py
@@ -0,0 +1,18 @@
+# Write a function that returns a string in which firstname is swapped with last name.
+
+# Example(Input --> Output)
+
+# "john McClane" --> "McClane john"
+
+
+
+# import codewars_test as test
+# from solution import name_shuffler
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(name_shuffler('john McClane'),'McClane john')
+#         test.assert_equals(name_shuffler('Mary jeggins'),'jeggins Mary')
+#         test.assert_equals(name_shuffler('tom jerry'),'jerry tom')
