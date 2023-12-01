82e439a1e2753ef28055e66102e5bad22f012eeb
tuyojr <olutuyod@gmail.com>
2023-12-01 08:41:38 +0100

    sample tests

---
+++ code_wars/grasshopper_debug.py
@@ -0,0 +1,12 @@
+# Grasshopper - Function syntax debugging
+# A student was working on a function and made some syntax 
+# mistakes while coding. Help them find their mistakes and fix them.
+# def main [verb, noun]
+# return verb + noun
+
+
+
+# @test.describe('Example Tests')
+# def example_tests():
+#     test.assert_equals(main('take ', 'item'), 'take item')
+#     test.assert_equals(main('use ', 'sword'), 'use sword')
