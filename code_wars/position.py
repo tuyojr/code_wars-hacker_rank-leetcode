1ecbd2404556ccf1acb73147c20c7dde624a9d8f
tuyojr <olutuyod@gmail.com>
2023-12-03 08:48:00 +0100

    find the position problem description

---
+++ code_wars/position.py
@@ -0,0 +1,8 @@
+# When provided with a letter, return its position in the alphabet.
+
+# Input :: "a"
+
+# Ouput :: "Position of alphabet: 1"
+
+# The ord() function takes a string that represents 1 Unicode character 
+# and returns an integer representing the Unicode code point of the given character.
