2690dc923778bc2f677818ee047faf349b337d81
tuyojr <olutuyod@gmail.com>
2023-12-03 08:49:58 +0100

    sample tests

--- code_wars/position.py
+++ code_wars/position.py
@@ -6,3 +6,23 @@
 
 # The ord() function takes a string that represents 1 Unicode character 
 # and returns an integer representing the Unicode code point of the given character.
+
+
+
+# import codewars_test as test
+# from solution import position
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+
+#         tests = [
+#             # [input, expected]
+#             ["a", "Position of alphabet: 1"],
+#             ["z", "Position of alphabet: 26"],
+#             ["e", "Position of alphabet: 5"],
+#         ]
+        
+#         for inp, exp in tests:
+#             test.assert_equals(position(inp), exp)
